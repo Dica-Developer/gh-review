7fec21dd7ee8c19f16b4aa8faeae5c98a4761ad6
Jörg Weber <weber_joerg.h@web.de>
2013-10-15 20:24:01 +0200

    * add method t get commit comments

--- app/js/commits/commit-model.js
+++ app/js/commits/commit-model.js
@@ -21,22 +21,42 @@ define(['backbone', 'app', 'when'], function(Backbone, app, when){
 
       return defer.promise;
     },
+    getCommitComments: function(){
+      var _this = this;
+      var defer = when.defer();
+      app.github.repos.getCommitComments({
+        user: app.currentReviewData.user,
+        repo: app.currentReviewData.repo,
+        sha: _this.get('sha')
+      },function(error, resp){
+        console.log(error, resp);
+        defer.resolve();
+      });
+      return defer.promise;
+    },
     addLineComment: function(fileIndex, position, comment){
+      var defer = when.defer();
       var diff = this.get('diff');
       var files = diff.files;
       var file = files[fileIndex];
       app.github.repos.createCommitComment({
-        user: this.get('user'),
-        repo: this.get('repo'),
+        user: app.currentReviewData.user,
+        repo: app.currentReviewData.repo,
         sha: this.get('sha'),
         body: comment,
         /* jshint camelcase:false */
         commit_id: file.sha,
         path: file.filename,
         position: position
-      }, function(error, resp){
-        console.log(error, resp);
+      }, function(error/*, resp*/){
+        if(!error){
+          defer.resolve();
+        } else {
+          defer.reject();
+          app.logger.error(error);
+        }
       });
+      return defer.promise;
     }
   });
 
