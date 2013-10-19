418f9fa936c4cbd83420249a4e02e4343cf3bdaf
Jörg Weber <weber_joerg.h@web.de>
2013-10-19 13:59:12 +0200

    * move html attribute adding into template

--- app/js/commits/commit-list-item-view.js
+++ app/js/commits/commit-list-item-view.js
@@ -2,15 +2,12 @@
 define(['backbone', 'underscore', 'text!../templates/commit-list-item.html'], function(Backbone, _, template){
   'use strict';
   var CommitListView = Backbone.View.extend({
-    tagName: 'a',
     template: _.template(template),
     initialize: function(){
-      this.$el.attr('href', '#commit/' + this.model.cid);
-      this.$el.addClass('list-group-item');
       this.render();
     },
     render: function(){
-      this.$el.html(this.template(this.model.toJSON()));
+      return this.template(this.model.toJSON());
     }
   });
 
