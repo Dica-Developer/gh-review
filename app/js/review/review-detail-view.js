a4244ab0b6a1c09359cfd3f9735b7290cc02d04f
Jörg Weber <weber_joerg.h@web.de>
2013-10-20 04:54:56 +0200

    * remove last page link form commit list

--- app/js/review/review-detail-view.js
+++ app/js/review/review-detail-view.js
@@ -13,10 +13,9 @@ define([
     el: '#main',
     template: _.template(template),
     events: {
-      'click .previous.page': 'getPreviousPage',
-      'click .previous.first': 'getFirstPage',
-      'click .next.page': 'getNextPage',
-      'click .next.last': 'getLastPage'
+      'click .previous': 'getPreviousPage',
+      'click .next': 'getNextPage',
+      'click .first': 'getFirstPage'
     },
     initialize: function(){
       app.currentReviewData = {
@@ -30,7 +29,6 @@ define([
       this.model.set('hasNext', app.github.hasNextPage(commits.meta.link));
       this.model.set('hasPrevious', app.github.hasPreviousPage(commits.meta.link));
       this.model.set('hasFirst', app.github.hasFirstPage(commits.meta.link));
-      this.model.set('hasLast', app.github.hasLastPage(commits.meta.link));
       this.model.set('currentLink', commits.meta.link);
     },
     getCommits: function(){
@@ -55,17 +53,6 @@ define([
         }
       });
     },
-    getFirstPage: function(){
-      var _this = this;
-      app.github.getFirstPage(this.model.get('currentLink'), function(error, commits){
-        if(!error){
-          _this.storeMetaToModel(commits);
-          commitCollection.reset(commits);
-          _this.render();
-          _this.renderAllCommits();
-        }
-      });
-    },
     getNextPage: function(){
       var _this = this;
       app.github.getNextPage(this.model.get('currentLink'), function(error, commits){
@@ -77,9 +64,9 @@ define([
         }
       });
     },
-    getLastPage: function(){
+    getFirstPage: function(){
       var _this = this;
-      app.github.getLastPage(this.model.get('currentLink'), function(error, commits){
+      app.github.getFirstPage(this.model.get('currentLink'), function(error, commits){
         if(!error){
           _this.storeMetaToModel(commits);
           commitCollection.reset(commits);
