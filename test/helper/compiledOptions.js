f37bfb3ca94f5b9fbe18bb24b626db12b97429f1
Jörg Weber <weber.joerg.h@gmail.com>
2014-02-07 22:50:23 +0100

    * add compiled options to test helper

---
+++ test/helper/compiledOptions.js
@@ -0,0 +1,10 @@
+/*global define*/
+define([], function(){
+  'use strict';
+  return {
+    clientId: '12345',
+    apiScope: 'user, repo',
+    redirectUri: 'http://dontexist.com',
+    accessTokenUrl: 'http://dontexist.com/auth'
+  };
+});
