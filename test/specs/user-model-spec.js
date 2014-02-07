69e9d04938ffd932ef781601d4f9066595a4f234
Jörg Weber <weber.joerg.h@gmail.com>
2014-02-07 22:51:51 +0100

    * add user model spec

---
+++ test/specs/user-model-spec.js
@@ -0,0 +1,56 @@
+/*global define, describe, it, expect, afterEach, beforeEach, spyOn, waitsFor, runs, sinon, localStorage, jasmine*/
+define(['server', 'githubRequests', 'app', 'userModel'], function(server, githubRequests, app, UserModel){
+  'use strict';
+
+  describe('OAuth', function(){
+
+    it('Should be defined', function(){
+      expect(UserModel).toBeDefined();
+    });
+
+    describe('new UserModel()', function(){
+
+      it('Should not call github api until app triggers ready event', function(){
+        var githubUserGetSpy = spyOn(app.github.user, 'get');
+        new UserModel();
+        expect(githubUserGetSpy).not.toHaveBeenCalled();
+      });
+
+      it('Should call github api if app triggers ready event', function(){
+        var githubGithubUserSpy = spyOn(app.github.user, 'get');
+        new UserModel();
+        app.trigger('ready');
+        expect(githubGithubUserSpy).toHaveBeenCalled();
+      });
+
+      it('After github api call handleResponse should be called', function(){
+        server.githubUserGet();
+        var userModel = new UserModel();
+        var handleResponseSpy = spyOn(userModel, 'handleResponse');
+        userModel.getUserData();
+
+        waitsFor(function(){
+          return server.server.requests[0].readyState === 4;
+        }, '', 500);
+
+        runs(function(){
+          var expectedResponse = githubRequests.userGet;
+          expectedResponse.meta = {};
+          expect(handleResponseSpy).toHaveBeenCalledWith(null, expectedResponse);
+          server.stop();
+        });
+
+      });
+
+      it('.handleResponse should store res', function(){
+        var userModel = new UserModel();
+        var expectedResponse = githubRequests.userGet;
+        userModel.handleResponse(null, expectedResponse);
+        expect(app.user).toBe(expectedResponse);
+        expect(userModel.get('login')).toBe('USER');
+      });
+    });
+
+  });
+
+});
