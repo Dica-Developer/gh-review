182d95bacf36598cc77fb09d132fa36042ef882d
Jörg Weber <weber.joerg.h@gmail.com>
2014-02-07 22:50:44 +0100

    * add sample github responses to test helper

---
+++ test/helper/githubRequests.js
@@ -0,0 +1,44 @@
+/*global define*/
+define(function(){
+  'use strict';
+
+  return {
+    userGet: {
+      'login': 'USER',
+      'id': 675206,
+      'avatar_url': 'https://gravatar.com/avatar/',
+      'gravatar_id': '',
+      'url': 'https://api.github.com/users/USER',
+      'html_url': 'https://github.com/USER',
+      'followers_url': 'https://api.github.com/users/USER/followers',
+      'following_url': 'https://api.github.com/users/USER/following{/other_user}',
+      'gists_url': 'https://api.github.com/users/USER/gists{/gist_id}',
+      'starred_url': 'https://api.github.com/users/USER/starred{/owner}{/repo}',
+      'subscriptions_url': 'https://api.github.com/users/USER/subscriptions',
+      'organizations_url': 'https://api.github.com/users/USER/orgs',
+      'repos_url': 'https://api.github.com/users/USER/repos',
+      'events_url': 'https://api.github.com/users/USER/events{/privacy}',
+      'received_events_url': 'https://api.github.com/users/USER/received_events',
+      'type': 'User',
+      'site_admin': false,
+      'name': 'User User',
+      'company': null,
+      'blog': null,
+      'location': null,
+      'email': null,
+      'hireable': false,
+      'bio': null,
+      'public_repos': 2,
+      'public_gists': 0,
+      'followers': 4,
+      'following': 3,
+      'created_at': '2011-03-17T14:10:35Z',
+      'updated_at': '2014-02-07T19:24:11Z',
+      'private_gists': 0,
+      'total_private_repos': 3,
+      'owned_private_repos': 3,
+      'disk_usage': 86708,
+      'collaborators': 3
+    }
+  };
+});
