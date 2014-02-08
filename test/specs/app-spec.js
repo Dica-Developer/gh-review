17bafa10028b3b0f1c7489f1ed7ebb5c763684ed
Jörg Weber <weber.joerg.h@gmail.com>
2014-02-08 02:19:55 +0100

    * add app spec

---
+++ test/specs/app-spec.js
@@ -0,0 +1,85 @@
+/*global define, describe, it, expect, spyOn, localStorage, beforeEach, afterEach*/
+define(['underscore', 'app', 'GitHub', 'OAuth'], function(_, app, GitHub, OAuth){
+  'use strict';
+
+  describe('#GH-Review', function(){
+
+    it('Should be defined', function(){
+      expect(app).toBeDefined();
+    });
+
+    it('Should call #GH-Review.authenticate if localStorage "inAuthorizationProcess" is present', function(){
+      var tmpApp = _.extend({}, app);
+      var authenticateSpy = spyOn(tmpApp, 'authenticate');
+      localStorage.inAuthorizationProcess = true;
+      tmpApp.init();
+      localStorage.removeItem('inAuthorizationProcess');
+      expect(authenticateSpy).toHaveBeenCalled();
+    });
+
+    describe('‘GH-Review.authenticate', function(){
+
+      var tmpApp = null;
+
+      beforeEach(function(){
+        tmpApp = _.extend({}, app);
+      });
+
+      afterEach(function(){
+        tmpApp = null;
+      });
+
+      it('Should request oauth token', function(){
+        var requestAccessTokenSpy = spyOn(OAuth.prototype, 'getAuthorizationCode');
+        tmpApp.authenticate();
+        expect(requestAccessTokenSpy).toHaveBeenCalled();
+      });
+
+      it('Should override .onAccessTokenReceived', function(){
+        spyOn(OAuth.prototype, 'getAuthorizationCode');
+        tmpApp.authenticate();
+        expect(tmpApp.oauth.onAccessTokenReceived).not.toEqual(OAuth.prototype.onAccessTokenReceived);
+      });
+
+      it('#GH-Review.ouath should be instance of OAuth', function(){
+        spyOn(OAuth.prototype, 'getAuthorizationCode');
+        tmpApp.authenticate();
+        expect(tmpApp.oauth instanceof OAuth).toBeTruthy();
+      });
+
+      describe('#OAuth.onAccessTokenReceived', function(){
+
+        var tmpApp = null;
+
+        beforeEach(function(){
+          tmpApp = _.extend({}, app);
+        });
+
+        afterEach(function(){
+          tmpApp = null;
+        });
+
+        it('Should call #GitHub.authenticate', function(){
+          var authenticateSpy = spyOn(GitHub.prototype, 'authenticate');
+          spyOn(OAuth.prototype, 'getAuthorizationCode');
+          tmpApp.authenticate();
+          tmpApp.oauth.accessToken = 'testToken';
+          tmpApp.oauth.onAccessTokenReceived();
+          expect(authenticateSpy).toHaveBeenCalledWith({ type : 'token', token : 'testToken' });
+        });
+
+        it('Should set #GH-Review.authenticated to true', function(){
+          spyOn(GitHub.prototype, 'authenticate');
+          spyOn(OAuth.prototype, 'getAuthorizationCode');
+          tmpApp.authenticate();
+          tmpApp.oauth.onAccessTokenReceived();
+          expect(tmpApp.authenticated).toBeTruthy();
+        });
+
+      });
+
+    });
+
+  });
+
+});
