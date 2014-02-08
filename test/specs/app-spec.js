74cb45d7fda58d3086dba57c2c218d3a50e45e12
Jörg Weber <weber.joerg.h@gmail.com>
2014-02-08 11:13:23 +0100

    * test ajax indicator

--- test/specs/app-spec.js
+++ test/specs/app-spec.js
@@ -1,5 +1,5 @@
-/*global define, describe, it, expect, spyOn, localStorage, beforeEach, afterEach*/
-define(['underscore', 'app', 'GitHub', 'OAuth'], function(_, app, GitHub, OAuth){
+/*global define, describe, it, expect, spyOn, localStorage, beforeEach, afterEach, jasmine*/
+define(['jquery', 'underscore', 'app', 'GitHub', 'OAuth', 'bootstrap'], function($, _, app, GitHub, OAuth){
   'use strict';
 
   describe('#GH-Review', function(){
@@ -76,6 +76,55 @@ define(['underscore', 'app', 'GitHub', 'OAuth'], function(_, app, GitHub, OAuth)
           expect(tmpApp.authenticated).toBeTruthy();
         });
 
+      });
+
+    });
+
+    describe('.showIndicator', function(){
+      var indicator = null, tmpApp = null;
+
+      beforeEach(function(){
+        tmpApp = _.extend({}, app);
+        indicator = $('<div id="ajaxIndicator" class="modal fade"><div class="modal-dialog">'+
+          '<div class="modal-content"><div class="modal-body"><p>Fetching results ... </p>' +
+          '</div></div></div></div>');
+        tmpApp.ajaxIndicator = indicator.modal({
+          backdrop: true,
+          show: false,
+          keyboard: false
+        });
+      });
+
+      afterEach(function(){
+        indicator.remove();
+        tmpApp = null;
+      });
+
+      it('Should be hidden per default', function(){
+        expect(indicator.is(':visible')).toBeFalsy();
+      });
+
+      it('Should be visible after 700ms timeout', function(){
+        jasmine.Clock.useMock();
+        tmpApp.showIndicator(true);
+        expect(indicator.is(':visible')).toBeFalsy();
+        jasmine.Clock.tick(500);
+        expect(indicator.is(':visible')).toBeFalsy();
+        jasmine.Clock.tick(701);
+        expect(indicator.is(':visible')).toBeTruthy();
+      });
+
+      it('Should be closed if passing "false" and indicator is visible', function(){
+        jasmine.Clock.useMock();
+        tmpApp.showIndicator(true);
+        expect(indicator.is(':visible')).toBeFalsy();
+        jasmine.Clock.tick(500);
+        expect(indicator.is(':visible')).toBeFalsy();
+        jasmine.Clock.tick(701);
+        expect(indicator.is(':visible')).toBeTruthy();
+        tmpApp.showIndicator(false);
+        jasmine.Clock.tick(720);
+        expect(indicator.is(':visible')).toBeFalsy();
       });
 
     });
