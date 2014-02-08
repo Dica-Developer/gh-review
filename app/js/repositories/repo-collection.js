444217e1120cea166e0595b82cadff7416f1156c
Jörg Weber <weber.joerg.h@gmail.com>
2014-02-08 12:46:53 +0100

    * refactor repo collection

--- app/js/repositories/repo-collection.js
+++ app/js/repositories/repo-collection.js
@@ -9,32 +9,33 @@ define([
 
   var RepoCollection = Backbone.Collection.extend({
     model: repoModel,
+    organizations: {},
     initialize: function () {
       this.getRepos();
     },
     getRepos: function () {
-      var _this = this;
-      app.github.repos.getAll({}, function (error, res) {
-        _this.reset(res);
-      });
-
-      app.github.user.getOrgs({}, function (error, res) {
-        _this.getOrgRepos(res);
-      });
+      app.github.repos.getAll({}, this.getAllReposCallback.bind(this));
+      app.github.user.getOrgs({}, this.getOrgsCallback.bind(this));
     },
     getOrgRepos: function (res) {
-      var _this = this;
       _.forEach(res, function (org) {
-        var organization = {
-          organization: org
-        };
-        app.github.repos.getFromOrg({'org': org.login, 'type': 'all'}, function (error, res) {
-          _.forEach(res, function (repo) {
-            _.extend(repo, organization);
-            _this.add(repo);
-          });
-        });
-      });
+        this.organizations[org.login] = org;
+        app.github.repos.getFromOrg({'org': org.login, 'type': 'all'}, this.getRepoFromOrgCallback.bind(this));
+      }, this);
+    },
+    getAllReposCallback: function (error, res) {
+      this.reset(res);
+    },
+    getOrgsCallback: function (error, res) {
+      this.getOrgRepos(res);
+    },
+    getRepoFromOrgCallback: function (error, res) {
+      _.forEach(res, function (repo) {
+        if(!_.isUndefined(this.organizations[repo.owner.login])){
+          _.extend(repo, {organization: this.organizations[repo.owner.login]});
+        }
+        this.add(repo);
+      }, this);
     }
   });
 
