f12d64588838b668c165bd16312d6a82cab47226
Jörg Weber <weber.joerg.h@gmail.com>
2014-02-08 11:13:09 +0100

    * add karma less preprocessor for testing with styles applied

--- test/dev.karma.conf.js
+++ test/dev.karma.conf.js
@@ -8,6 +8,7 @@ module.exports = function(config) {
     files: [
       {pattern: 'app/js/**/*.js', included: false},
       {pattern: 'app/bower_components/**/*.js', included: false},
+      {pattern: 'app/bower_components/bootstrap/less/bootstrap.less'},
       {pattern: 'test/specs/**/*-spec.js', included: false},
       {pattern: 'test/helper/**/*.js', included: false},
       {pattern: 'test/lib/**/*.js', included: true},
@@ -18,7 +19,17 @@ module.exports = function(config) {
     browsers: ['PhantomJS'],
     reporters: ['dots', 'coverage'],
     preprocessors: {
-      'app/js/*.js': ['coverage']
+      'app/js/*.js': ['coverage'],
+      'app/bower_components/bootstrap/less/*.less': ['less']
+    },
+    lessPreprocessor:{
+      options: {
+        paths: ['app/bower_components/bootstrap/less'],
+        save: true,
+        transformPath: function(path) {
+          return path.replace(/\.less$/, '.compiled.css');
+        }
+      }
     },
     coverageReporter: {
       type : 'html',
