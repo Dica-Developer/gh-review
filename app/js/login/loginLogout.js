ea5e6dece9b68715cb680eeaf721906b280163ea
Jörg Weber <weber.joerg.h@gmail.com>
2014-02-13 14:09:37 +0100

    * add login logout tests

--- app/js/login/loginLogout.js
+++ app/js/login/loginLogout.js
@@ -8,25 +8,20 @@ define([
     return (localStorage !== 'undefined');
   }
 
-  function login(){
-    app.router.navigate('#oauth/accesstoken', {
-      trigger: true
-    });
-  }
-
-  function redirectToRoot() {
-    window.location.href = window.location.protocol + '//' + window.location.host + window.location.pathname;
-  }
-
-  function logout(){
-    if(hasLocalStorage()){
-      localStorage.removeItem('accessToken');
-    }
-    redirectToRoot();
-  }
-
   return {
-    login: login,
-    logout: logout
+    login: function login(){
+      app.router.navigate('#oauth/accesstoken', {
+        trigger: true
+      });
+    },
+    logout: function logout(){
+      if(hasLocalStorage()){
+        localStorage.removeItem('accessToken');
+      }
+      this.redirectToRoot();
+    },
+    redirectToRoot: function redirectToRoot() {
+      window.location.href = window.location.protocol + '//' + window.location.host + window.location.pathname;
+    }
   };
 });
