393ffde951606859b00c636a5d9d9fd41f8b972d
Martin Schaaf <mschaaf@datameer.com>
2014-02-19 19:02:39 +0100

    * close ajax indicator spinner on user page

--- app/js/whoami-view.js
+++ app/js/whoami-view.js
@@ -1,12 +1,15 @@
 /*global define*/
-define(['backbone', 'underscore', 'app', 'text!templates/whoami.html'], function(Backbone, _, app, template){
+define(['backbone', 'underscore', 'app', 'text!templates/whoami.html'], function (Backbone, _, app, template) {
   'use strict';
 
   return Backbone.View.extend({
     el: '#main',
     template: _.template(template),
-    render: function(){
-      this.$el.html(this.template({user: app.user}));
+    render: function () {
+      this.$el.html(this.template({
+        user: app.user
+      }));
+      app.showIndicator(false);
     }
   });
 
