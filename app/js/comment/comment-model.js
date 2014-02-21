b42ade7d94e2ec3b38bff583cf8390060395ca03
Sebastian Froehlich <sfroehlich@datameer.com>
2014-02-21 23:06:25 +0100

    fix #50 Hide edit/delete button if comment owner is not the authorized user

--- app/js/comment/comment-model.js
+++ app/js/comment/comment-model.js
@@ -1,11 +1,13 @@
 /* global define */
-define(['backbone', 'moment'], function (Backbone, moment) {
+define(['backbone', 'moment', 'app'], function (Backbone, moment, app) {
   'use strict';
 
   var CommentModel = Backbone.Model.extend({
     initialize: function () {
       var date = moment(this.get('created_at'));
       this.set('commentFromNow', date.fromNow());
+      var isCommentEditable = this.get('user').login === app.user.login;
+      this.set('isCommentEditable', isCommentEditable);
       var id = this.get('id');
       this.set('commentId', id);
     }
