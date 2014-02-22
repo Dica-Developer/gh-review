fbc4ab6a5ca3196bd4e9a7785e3e4a25e95debe8
Sebastian Froehlich <sfroehlich@datameer.com>
2014-02-22 21:28:53 +0100

    * review comment * handle possible NPE if user is not authenticated

--- app/js/comment/comment-model.js
+++ app/js/comment/comment-model.js
@@ -6,7 +6,10 @@ define(['backbone', 'moment', 'app'], function (Backbone, moment, app) {
     initialize: function () {
       var date = moment(this.get('created_at'));
       this.set('commentFromNow', date.fromNow());
-      var isCommentEditable = this.get('user').login === app.user.login;
+      var isCommentEditable = false;
+      if (app.authenticated) {
+        isCommentEditable = this.get('user').login === app.user.login;
+      }
       this.set('isCommentEditable', isCommentEditable);
       var id = this.get('id');
       this.set('commentId', id);
