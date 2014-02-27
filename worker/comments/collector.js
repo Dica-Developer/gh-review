8330c896ee44a89a948e11eac49caa8556d2fa1a
Martin Schaaf <mschaaf@datameer.com>
2014-02-27 09:39:35 +0100

    * deploy release

--- worker/comments/collector.js
+++ worker/comments/collector.js
@@ -1,9 +1,9 @@
-/*global onmessage, XMLHttpRequest, postMessage, analyzeComments*/
+/*global onmessage:true, XMLHttpRequest, postMessage, analyzeComments*/
 var _accessToken = null;
 
 var _urls = null;
 
-function getPageLinks(link) {
+function getPageLinks (link) {
   'use strict';
   var links = {};
   link.replace(/<([^>]*)>;\s*rel="([\w]*)\"/g, function (m, uri, type) {
@@ -12,12 +12,12 @@ function getPageLinks(link) {
   return links;
 }
 
-function errorCallback() {
+function errorCallback () {
   'use strict';
   // TODO post to application and print in UI
 }
 
-function successCallback(event) {
+function successCallback (event) {
   'use strict';
   var xhr = event.currentTarget;
   var links = xhr.getResponseHeader('Link');
@@ -37,7 +37,7 @@ function successCallback(event) {
   });
 }
 
-function analyzeComments(url) {
+function analyzeComments (url) {
   'use strict';
   var xhr = new XMLHttpRequest();
   xhr.open('GET', url, true);
@@ -49,10 +49,10 @@ function analyzeComments(url) {
   xhr.send();
 }
 
-function start() {
+function start () {
   'use strict';
   var idx = 0;
-  for (idx = 0; _urls !== null && idx < _urls.length; idx++) {
+  for (; _urls !== null && idx < _urls.length; idx++) {
     analyzeComments(_urls[idx]);
   }
 }
