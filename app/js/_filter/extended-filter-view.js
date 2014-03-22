5d61aaadb6bee113571dd6c403ff0f1a24fe4602
Jörg Weber <weber.joerg.h@gmail.com>
2014-03-23 00:53:43 +0100

    * add ability to add until/since to filter

--- app/js/_filter/extended-filter-view.js
+++ app/js/_filter/extended-filter-view.js
@@ -1,4 +1,4 @@
-/*global define*/
+/*global define, crossfilter, d3*/
 define([
   'jquery',
   'backbone',
@@ -13,35 +13,148 @@ define([
 
   return Backbone.View.extend({
     el: '#extendedFilterView',
+    /*jshint camelcase:false  */
     commitFilter: {
       user: '',
-      repo: ''
+      repo: '',
+      page: 1,
+      per_page: 100
     },
     events: {
+      'change #timeChartMethod': 'changeTimeChartMethod',
+      'click #applyTimeChartSettings': 'applyTimeChartSettings'
     },
     template: _.template(template),
+    commits: [],
+    getCommitDefer: null,
     initialize: function () {
-      console.log(this.model.toJSON());
+      var twoWeeksAgo = moment().subtract('weeks', 2).toISOString();
       this.commitFilter.repo = this.model.get('name');
       this.commitFilter.user = this.model.get('owner').login;
-      app.github.repos.getCommits(this.commitFilter, function(err, resp){
-        console.log(resp);
-      });
+      this.commitFilter.since = twoWeeksAgo;
+      this.getCommitsWithCurrentFilter()
+        .then(this.processData.bind(this));
       this.model.getAdditionalInformations()
         .then(this.renderBranchesAndContributors.bind(this));
       this.render();
     },
-    renderBranchesAndContributors: function(){
-      var branches = this.model.get('branches');
-      var contributors = this.model.get('contributors');
+    applyTimeChartSettings: function(){
+      var method = this.$('#timeChartMethod').find(':selected').val();
+      if('sliding' === method){
+        var amount = this.$('#timeChartSlidingAmount').val();
+        var pattern = this.$('#timeChartSlidingPattern').find(':selected').val();
+        this.commitFilter.since = moment().subtract(pattern, amount).toISOString();
+      } else {
+        var start = this.$('#timeChartFixedStart').val();
+        var end = this.$('#timeChartFixedEnd').val();
+        if('' !== start){
+          this.commitFilter.since = moment(start).toISOString();
+        }
+        if('' !== end){
+          this.commitFilter.until = moment(end).toISOString();
+        }
+      }
+      this.commits = [];
+      this.getCommitsWithCurrentFilter()
+        .then(this.processData.bind(this));
+    },
+    changeTimeChartMethod: function(event){
+      var target = this.$(event.target);
+      var value = target.find(':selected').val();
+      if('sliding' === value){
+        this.$('#timeChartFixedSettings').hide();
+        //.show() doesn't work here because .show() sets display to 'block' but we need 'inline-block'
+        this.$('#timeChartSlidingSettings').css('display', 'inline-block');
+      }else{
+        this.$('#timeChartSlidingSettings').hide();
+        //.show() doesn't work here because .show() sets display to 'block' but we need 'inline-block'
+        this.$('#timeChartFixedSettings').css('display', 'inline-block');
+      }
+    },
+    getCommitsWithCurrentFilter: function (link) {
+      var callback = function (error, resp) {
+        if (!error) {
+          var link = resp.meta.link;
+          var hasNext = app.github.hasNextPage(link);
+          delete resp.meta;
+          this.commits = this.commits.concat(resp);
+          if (hasNext) {
+            this.getCommitsWithCurrentFilter(link);
+          } else {
+            this.getCommitDefer.resolve();
+          }
+        }
+      }.bind(this);
+      if (!link) {
+        this.getCommitDefer = when.defer();
+        app.github.repos.getCommits(this.commitFilter, callback);
+      } else {
+        app.github.getNextPage(link, callback);
+      }
+      return this.getCommitDefer.promise;
+    },
+    renderBranchesAndContributors: function () {
+//      var branches = this.model.get('branches');
+//      var contributors = this.model.get('contributors');
+    },
+    processData: function () {
+      var rawData = _.clone(this.commits);
+      _.each(rawData, function (data) {
+        data.commitDate = new Date(data.commit.author.date);
+        data.commitDay = d3.time.day(data.commitDate);
+      }, this);
+      var data = crossfilter(rawData);
+//        var all = data.groupAll();
 
-      console.log(branches, contributors);
+      var sortedDate = _.sortBy(rawData, function (data) {
+        return data.commitDate.getTime();
+      });
+
+      var smallestGreatestDateOfCommits = {
+        smallest: sortedDate[0].commitDate,
+        greatest: sortedDate[sortedDate.length - 1].commitDate
+      };
+
+      var commitsByDay = data.dimension(function (d) {
+        return d.commitDay;
+      });
+      var commitsByDayGroup = commitsByDay.group();
+
+      this.renderTimeChart(commitsByDay, commitsByDayGroup, smallestGreatestDateOfCommits);
+    },
+    renderTimeChart: function (commitsByDay, commitsByDayGroup, smallestGreatestDateOfCommits) {
+      var availableWidth = this.$('#timeWindowFilter').width();
+      dc.barChart('#time-chart')
+        .width(availableWidth)
+        .height(150)
+        .margins({top: 20, right: 0, bottom: 20, left: 30})
+        .renderHorizontalGridLines(true)
+        .dimension(commitsByDay)
+        .group(commitsByDayGroup)
+        .centerBar(true)
+        .mouseZoomable(false)
+        .round(d3.time.day.round)
+        .alwaysUseRounding(true)
+        .x(d3.time.scale().domain([smallestGreatestDateOfCommits.smallest, smallestGreatestDateOfCommits.greatest]))
+        .xUnits(d3.time.day)
+        .brushOn(false)
+        .render();
+    },
+    applyHelperTooltips: function(){
+      var tooltipOptions = {
+        placement: 'top',
+        trigger: 'hover',
+        delay: { show: 500, hide: 100 }
+      };
+      var timeChartFilterSinceTooltip = _.extend({title: 'Optional: If you leave blank and only until'}, tooltipOptions);
+      this.$('#timeChartFixedStartLabel').tooltip(timeChartFilterSinceTooltip);
     },
     render: function () {
       this.$el.html(this.template());
       this.$el.height($(window).height());
       var position = this.$el.position();
       $('body').scrollTop(position.top - 60);
+      this.applyHelperTooltips();
     }
   });
 
