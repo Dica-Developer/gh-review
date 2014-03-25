f57519b89db090392b7143c7f659687836378d07
Jörg Weber <weber.joerg.h@gmail.com>
2014-03-25 21:21:57 +0100

    * refactor to use new filter

--- app/js/_filter/extended-filter-view.js
+++ app/js/_filter/extended-filter-view.js
@@ -18,12 +18,7 @@ define([
     el: '#extendedFilterView',
     charts: new Charts(),
     /*jshint camelcase:false  */
-    commitFilter: {
-      user: '',
-      repo: '',
-      page: 1,
-      per_page: 100
-    },
+    filter: new FilterModel(),
     events: {
       'change #timeChartMethod': 'changeTimeChartMethod',
       'click #applyTimeChartSettings': 'applyTimeChartSettings',
@@ -35,28 +30,22 @@ define([
     getCommitDefer: null,
     initialize: function () {
       var twoWeeksAgo = moment().subtract('weeks', 2).toISOString();
-      this.commitFilter.repo = this.model.get('name');
-      this.commitFilter.user = this.model.get('owner').login;
-      this.commitFilter.since = twoWeeksAgo;
-      this.getCommitsWithCurrentFilter()
+      this.filter.setRepo(this.model.get('name'));
+      this.filter.setOwner(this.model.get('owner').login);
+      this.filter.setSince(twoWeeksAgo);
+      this.filter.getCommits()
         .then(this.processDataAndRenderCharts.bind(this));
       this.model.getAdditionalInformations()
         .then(this.renderBranchesAndContributors.bind(this));
       this.render();
     },
     showPreview: function () {
-      var model = new FilterModel(this.currentFilter);
-      model.set('hasNext', false);
-      model.set('hasPrevious', false);
-      model.set('hasFirst', false);
-      model.set('currentLink', false);
       var view = new CommitListView({
-        model: model,
+        model: this.filter,
         el: '#commitList'
       });
-      view.commitCollection = this.commits;
       view.render();
-      view.renderAllCommits();
+      view.getAllCommits();
       $('#peviewModal').modal('show');
     },
     applyTimeChartSettings: function () {
@@ -64,34 +53,30 @@ define([
       if ('sliding' === method) {
         var amount = this.$('#timeChartSlidingAmount').val();
         var pattern = this.$('#timeChartSlidingPattern').find(':selected').val();
-        this.commitFilter.since = moment().subtract(pattern, amount).toISOString();
+        this.filter.setSince(moment().subtract(pattern, amount).toISOString());
       } else {
-        if (this.commitFilter.since) {
-          delete this.commitFilter.since;
-        }
-        if (this.commitFilter.until) {
-          delete this.commitFilter.until;
-        }
+        this.filter.unsetSince();
+        this.filter.unsetUntil();
         var start = this.$('#timeChartFixedStart').val();
         var end = this.$('#timeChartFixedEnd').val();
         if ('' !== start) {
-          this.commitFilter.since = moment(start).toISOString();
+          this.filter.setSince(moment(start).toISOString());
         }
         if ('' !== end) {
-          this.commitFilter.until = moment(end).toISOString();
+          this.filter.setUntil(moment(end).toISOString());
         }
       }
-      this.getCommitsWithCurrentFilter()
+      this.filter.getCommits()
         .then(this.processDataAndRenderCharts.bind(this));
     },
     applyPathFilter: function () {
       var path = this.$('#path-filter-input').val();
       if ('' !== path) {
-        this.commitFilter.path = path;
-      } else if ('' === path && typeof this.commitFilter.path !== 'undefined') {
-        delete this.commitFilter.path;
+        this.filter.setPath(path);
+      } else if ('' === path) {
+        this.filter.unsetPath();
       }
-      this.getCommitsWithCurrentFilter()
+      this.filter.getCommits()
         .then(this.processDataAndRenderCharts.bind(this));
     },
     changeTimeChartMethod: function (event) {
@@ -107,35 +92,12 @@ define([
         this.$('#timeChartFixedSettings').css('display', 'inline-block');
       }
     },
-    getCommitsWithCurrentFilter: function (link) {
-      var callback = function (error, resp) {
-        if (!error) {
-          var link = resp.meta.link;
-          var hasNext = app.github.hasNextPage(link);
-          delete resp.meta;
-          this.commits.add(resp);
-          if (hasNext) {
-            this.getCommitsWithCurrentFilter(link);
-          } else {
-            this.getCommitDefer.resolve();
-          }
-        }
-      }.bind(this);
-      if (!link) {
-        this.commits.reset();
-        this.getCommitDefer = when.defer();
-        app.github.repos.getCommits(this.commitFilter, callback);
-      } else {
-        app.github.getNextPage(link, callback);
-      }
-      return this.getCommitDefer.promise;
-    },
     renderBranchesAndContributors: function () {
 //      var branches = this.model.get('branches');
 //      var contributors = this.model.get('contributors');
     },
-    processDataAndRenderCharts: function () {
-      var rawData = this.commits.toJSON();
+    processDataAndRenderCharts: function (commits) {
+      var rawData = commits.toJSON();
       this.charts.processCommitData(rawData);
       var treeRawData = this.model.get('tree').tree;
       this.charts.processTreeData(treeRawData);
