13adcecfb2baeda05f1abc818a7af399cc3c47e7
Jörg Weber <weber.joerg.h@gmail.com>
2014-03-25 18:37:20 +0100

    * improve regex to find test specs and not specs from 3rd party libs * fix path test require setup

--- test/test-main.js
+++ test/test-main.js
@@ -6,7 +6,7 @@
   /*jshint camelcase:false*/
   for (var file in window.__karma__.files) {
     if (window.__karma__.files.hasOwnProperty(file)) {
-      if (/-spec\.js$/.test(file)) {
+      if (/specs\S*-spec\.js$/.test(file)) {
         tests.push(file);
       }
     }
@@ -61,7 +61,7 @@
       RepoDetailView: '../app/js/repositories/repo-detail-view',
 
       commitModel: '../app/js/commits/commit-model',
-      commitCollection: '../app/js/commits/commit-collection',
+      CommitCollection: '../app/js/commits/commit-collection',
       commitListItemView: '../app/js/commits/commit-list-item-view',
       CommitListView: '../app/js/commits/commit-list-view',
 
