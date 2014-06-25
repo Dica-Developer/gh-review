62fb835c1bce959333e7e9da0a24fa31ac4ec2fd
Jörg Weber <weber.joerg.h@gmail.com>
2014-06-25 21:41:48 +0200

    * refactor collector worker * use Promises to resolve the async request and push result at once * use addEventListener for request events (load, err, …) * move methods to sort out approved comments and commits into worker because of the better performance * set fetch interval to 5min (300000ms)

--- app/js/worker/collector.js
+++ app/js/worker/collector.js
@@ -1,90 +1,152 @@
-/*global onmessage:true, XMLHttpRequest, postMessage, analyzeComments*/
-var _accessToken = null;
-
-var _urls = null;
-
-var lastUrlLength = 0;
-var eTags = null;
-var setETags = false;
-
-function getPageLinks(link) {
+/*global Promise, importScripts, _*/
+(function (worker) {
     'use strict';
-    var links = {};
-    link.replace(/<([^>]*)>;\s*rel="([\w]*)\"/g, function (m, uri, type) {
-        links[type] = uri;
-    });
-    return links;
-}
 
-var errorCallback = function () {
-    'use strict';
-    // TODO post to application and print in UI
-};
+    importScripts('../../bower_components/lodash/dist/lodash.min.js');
 
-var successCallback = function (event) {
-    'use strict';
-    var xhr = event.currentTarget;
-    if (xhr.status === 200) {
-        var links = xhr.getResponseHeader('Link');
-        if (links && links !== '' && links.indexOf(':') !== -1) {
-            var next = getPageLinks(links).next;
-            if (next) {
-                analyzeComments(next);
-            }
-        }
-        if (setETags) {
-            if (eTags === null) {
-                eTags = {};
-            }
-            eTags[xhr.myUrl] = xhr.getResponseHeader('ETag');
-        }
-        postMessage({
-            type: 'comments',
-            comments: xhr.response
+    var _accessToken = '';
+    var _urls = [];
+    var urlPromises = [];
+    var commentsForRepo = {};
+    var commitApproved = {};
+    var approveComments = {};
+    var timeout = null;
+
+    var getPageLinks = function (link) {
+        var links = {};
+        link.replace(/<([^>]*)>;\s*rel="([\w]*)\"/g, function (m, uri, type) {
+            links[type] = uri;
         });
-    }
-};
+        return links;
+    };
 
-function analyzeComments(url) {
-    'use strict';
-    var xhr = new XMLHttpRequest();
-    xhr.myUrl = url;
-    xhr.open('GET', url, true);
-    xhr.responseType = 'json';
-    xhr.setRequestHeader('content-type', 'application/json; charset=utf-8');
-    xhr.setRequestHeader('authorization', 'token ' + _accessToken);
-    if (eTags !== null && eTags[url]) {
-        xhr.setRequestHeader('If-None-Match', eTags[url]);
-    }
-    xhr.onerror = errorCallback;
-    xhr.onload = successCallback;
-    xhr.send();
-}
+    var askGithub = function (url, successCallback, errorCallback) {
+        var req = new XMLHttpRequest();
+        req.open('GET', url, true);
+        req.responseType = 'json';
+        req.setRequestHeader('authorization', 'token ' + _accessToken);
+        req.addEventListener('load', successCallback, false);
+        req.addEventListener('error', errorCallback, false);
+        req.addEventListener('abort', errorCallback, false);
+        req.send();
+    };
 
-function start() {
-    'use strict';
-    var idx = 0;
-    var length = _urls.length;
-    if (lastUrlLength !== length) {
-        lastUrlLength = length;
-        eTags = null;
-    }
-    for (; _urls !== null && idx < length; idx++) {
-        analyzeComments(_urls[idx]);
-        if (length - 1 === idx) {
-            setETags = true;
+    var sortOutApproveComments = function (comments) {
+        return new Promise(function (resolve) {
+            var commentsLength = comments.length;
+            var approveCommit = function (comment) {
+                /*jshint camelcase:false*/
+                if (true !== commitApproved[comment.commit_id]) {
+                    commitApproved[comment.commit_id] = true;
+                }
+                if (true !== approveComments[comment.id]) {
+                    approveComments[comment.id] = true;
+                }
+            };
+
+            _.each(comments, function (comment, idx) {
+                var commentBody = comment.body;
+                if (commentBody) {
+                    if (commentBody.indexOf('```json') > -1) {
+                        commentBody = commentBody.substring(7, (commentBody.length - 3));
+                        commentBody = JSON.parse(commentBody);
+                        if (true === commentBody.approved) {
+                            approveCommit(comment, idx);
+                        }
+                    }
+                }
+                if (idx + 1 === commentsLength) {
+                    resolve();
+                }
+            });
+        });
+    };
+
+    var analyzeComments = function () {
+        return new Promise(function (resolve) {
+            var sortOutPromises = [];
+            _.each(commentsForRepo, function (comments) {
+                sortOutPromises.push(sortOutApproveComments(comments));
+            });
+
+            Promise.all(sortOutPromises)
+                .then(resolve);
+        });
+    };
+
+
+    var getComments = function (url) {
+        return new Promise(function (resolve, reject) {
+            var successCallback = function (event) {
+                var req = event.currentTarget;
+                if (req.status === 200) {
+                    if (commentsForRepo[url].length > 0) {
+                        commentsForRepo[url] = commentsForRepo[url].concat(req.response);
+                    } else {
+                        commentsForRepo[url] = req.response;
+                    }
+
+                    var links = req.getResponseHeader('Link');
+                    if (links && links !== '' && links.indexOf(':') !== -1) {
+                        var next = getPageLinks(links).next;
+                        if (next) {
+                            askGithub(next, successCallback, reject);
+                        } else {
+                            resolve();
+                        }
+                    } else {
+                        resolve();
+                    }
+                }
+            };
+            askGithub(url, successCallback, reject);
+        });
+    };
+
+    var postResults = function () {
+        worker.postMessage({
+            type: 'commentsCollected',
+            commentsForRepo: commentsForRepo,
+            commitApproved: commitApproved,
+            approveComments: approveComments
+        });
+        timeout = setTimeout(start, 300000);
+    };
+
+    var start = function () {
+        clearTimeout(timeout);
+        var uniqUrls = _.uniq(_urls);
+        _.each(uniqUrls, function (url) {
+            commentsForRepo[url] = [];
+            urlPromises.push(getComments(url));
+        });
+        Promise.all(urlPromises)
+            .then(analyzeComments)
+            .then(postResults);
+    };
+
+    worker.onmessage = function (event) {
+        var type = event.data.type;
+        switch (type) {
+        case 'accessToken':
+            _accessToken = event.data.accessToken;
+            break;
+        case 'repositories':
+            _urls = event.data.repositories;
+            break;
+        case 'repository':
+            if(_.indexOf(_urls, event.data.repository) === -1){
+                _urls.push(event.data.repository);
+            }
+            break;
+        case 'start':
+            start();
+            break;
+        case 'pause':
+            clearTimeout(timeout);
+            break;
+        default:
+            throw new Error('Unknown event type: ' + type);
         }
-    }
-}
-
-onmessage = function (event) {
-    'use strict';
-    if ('token' === event.data.type) {
-        _accessToken = event.data.token;
-    } else if ('repositories' === event.data.type) {
-        _urls = event.data.repositories;
-        start();
-        // TODO use setTimeout and start it after the last analyzeComments call
-        setInterval(start, 60000);
-    }
-};
+    };
+}(this));
