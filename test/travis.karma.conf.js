9260c8fc22d767e8bd74c279ae495c03bfd5a9a8
Jörg Weber <weber.joerg.h@gmail.com>
2014-06-29 12:02:51 +0200

    * add template path to karma config files

--- test/travis.karma.conf.js
+++ test/travis.karma.conf.js
@@ -7,6 +7,7 @@ module.exports = function (config) {
             {pattern: 'test/unit/**/*.js', included: false},
             {pattern: 'app/bower_components/**/*.js', included: false},
             {pattern: 'test/unit/**/*Spec.js', included: false },
+            {pattern: 'app/templates/*.html', included: false},
             // needs to be last http://karma-runner.github.io/0.10/plus/requirejs.html
             'test/main-test.js'
         ],
