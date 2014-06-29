b63b85377fff05f84acf04a945ef5bf2fc76ad0b
Jörg Weber <weber.joerg.h@gmail.com>
2014-06-29 09:57:35 +0200

    * add Funtion.prototype.bind polyfill to test main because PhantomJS didn’t know about .bind()

--- test/main-test.js
+++ test/main-test.js
@@ -69,10 +69,33 @@ require([
     'lodash',
     'underscore.string',
     'angular'
-], function (d3, _, str, angular) {
+], function (d3, _, str) {
     'use strict';
     window.d3 = d3;
     _.str = str;
+
+
+    //bind polyfill for PhantomJS
+    if (!Function.prototype.bind) {
+        Function.prototype.bind = function (oThis) {
+            if (typeof this !== 'function') {
+                throw new TypeError('Function.prototype.bind - what is trying to be bound is not callable');
+            }
+
+            var aArgs = Array.prototype.slice.call(arguments, 1),
+                fToBind = this,
+                FNOP = function () {},
+                fBound = function () {
+                    return fToBind.apply(this instanceof FNOP && oThis ? this : oThis,
+                        aArgs.concat(Array.prototype.slice.call(arguments)));
+                };
+
+            FNOP.prototype = this.prototype;
+            fBound.prototype = new FNOP();
+
+            return fBound;
+        };
+    }
 
     require([
         'app',
