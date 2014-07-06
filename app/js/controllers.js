e744655b8e40f097976a748b674867e27dd1c70b
Jörg Weber <weber.joerg.h@gmail.com>
2014-07-06 12:37:00 +0200

    Fixes #78

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -51,22 +51,51 @@ define(['angular', 'lodash'], function (angular, _) {
         }])
 
         .controller('FilterListController', ['$scope', 'filter', function ($scope, filter) {
+            $scope.groupingOptions = [
+                {
+                    value: 'repo',
+                    label: 'Repository'
+                },
+                {
+                    value: 'state',
+                    label: 'Review State'
+                }
+            ];
+            $scope.selectedGrouping = $scope.groupingOptions[0];
             var getGroupedAndSortedFilter = function(){
                 var groupedFilter = _.groupBy(filter.getAll(), function (filter) {
-                    return filter.getRepo();
+                    var groupValue;
+                    switch($scope.selectedGrouping.value){
+                    case 'repo':
+                        groupValue = filter.getRepo();
+                        break;
+                    case 'state':
+                        groupValue = filter.getState();
+                        break;
+                    default:
+                        console.error('Value for filter group unknown: ' + $scope.selectedGrouping.value);
+                        groupValue = filter.getRepo();
+                        break;
+                    }
+                    return groupValue;
                 });
 
                 var sortedGroupedFilter = _.sortBy(groupedFilter, 'length');
                 return sortedGroupedFilter.reverse();
             };
 
-            $scope.filterList = getGroupedAndSortedFilter();
+            var updateFilterList = function(){
+                $scope.filterList = getGroupedAndSortedFilter();
+
+            };
             $scope.removeFilter = function (filterId, event) {
                 if (void 0 !== event) {
                     event.preventDefault();
                 }
                 filter.remove(filterId);
-                $scope.filterList = getGroupedAndSortedFilter();
+                updateFilterList();
             };
+            $scope.$watch('selectedGrouping', updateFilterList);
+            updateFilterList();
         }]);
 });
