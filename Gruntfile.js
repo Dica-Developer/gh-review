740e70e224efbb64936bba8cc40413471a1dfd90
Martin Schaaf <mschaaf@datameer.com>
2014-09-03 12:14:21 +0200

    * formatting * add again the deploy task * announce module as global variable

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,10 +1,10 @@
+/* global module*/
 module.exports = function (grunt) {
     'use strict';
 
     require('time-grunt')(grunt);
     require('load-grunt-tasks')(grunt);
     grunt.loadNpmTasks('grunt-karma-coveralls');
-
 
     var config = {
         app: 'app',
@@ -13,7 +13,7 @@ module.exports = function (grunt) {
         test: 'test',
         distOptions: {
             clientId: '833c028df47be8e881d9',
-            apiScope: 'user, public_repo',
+            apiScope: 'user, repo',
             redirectUri: 'http://dica-developer.github.io/gh-review/',
             accessTokenUrl: 'http://gh-review.herokuapp.com/login/oauth/access_token'
         },
@@ -60,24 +60,20 @@ module.exports = function (grunt) {
         },
         clean: {
             dist: {
-                files: [
-                    {
-                        dot: true,
-                        src: [
-                            '<%= config.dist %>/*'
-                        ]
-                    }
-                ]
+                files: [{
+                    dot: true,
+                    src: [
+                        '<%= config.dist %>/*'
+                    ]
+                }]
             },
             dev: {
-                files: [
-                    {
-                        dot: true,
-                        src: [
-                            '<%= config.dev %>/*'
-                        ]
-                    }
-                ]
+                files: [{
+                    dot: true,
+                    src: [
+                        '<%= config.dev %>/*'
+                    ]
+                }]
             }
         },
         jshint: {
@@ -106,70 +102,58 @@ module.exports = function (grunt) {
         },
         copy: {
             deploy: {
-                files: [
-                    {
-                        expand: true,
-                        cwd: './dist',
-                        dest: '/tmp/gh-review.pages',
-                        src: '**'
-                    }
-                ]
+                files: [{
+                    expand: true,
+                    cwd: './dist',
+                    dest: '/tmp/gh-review.pages',
+                    src: '**'
+                }]
             },
             dev: {
-                files: [
-                    {
-                        expand: true,
-                        cwd: '<%= config.app %>',
-                        dest: '<%= config.dev %>',
-                        src: '**'
-                    },
-                    {
-                        expand: true,
-                        cwd: '<%= config.app %>/bower_components/bootstrap/dist/fonts',
-                        dest: '<%= config.dev %>/fonts',
-                        src: '*'
-                    },
-                    {
-                        expand: true,
-                        cwd: '<%= config.app %>/bower_components/requirejs',
-                        dest: '<%= config.dev %>/js',
-                        src: 'require.js'
-                    }
-                ]
+                files: [{
+                    expand: true,
+                    cwd: '<%= config.app %>',
+                    dest: '<%= config.dev %>',
+                    src: '**'
+                }, {
+                    expand: true,
+                    cwd: '<%= config.app %>/bower_components/bootstrap/dist/fonts',
+                    dest: '<%= config.dev %>/fonts',
+                    src: '*'
+                }, {
+                    expand: true,
+                    cwd: '<%= config.app %>/bower_components/requirejs',
+                    dest: '<%= config.dev %>/js',
+                    src: 'require.js'
+                }]
             },
             dist: {
-                files: [
-                    {
-                        expand: true,
-                        cwd: '<%= config.app %>',
-                        dest: '<%= config.dist %>',
-                        src: ['js/worker/*', 'img/**/*', 'templates/**/*', 'fonts/**/*', '*.html']
-                    },
-                    {
-                        expand: true,
-                        cwd: '<%= config.app %>/bower_components/bootstrap/dist/fonts',
-                        dest: '<%= config.dist %>/fonts',
-                        src: '*'
-                    },
-                    {
-                        expand: true,
-                        cwd: '<%= config.app %>/bower_components/requirejs',
-                        dest: '<%= config.dist %>/js',
-                        src: 'require.js'
-                    },
-                    {
-                        expand: true,
-                        cwd: '<%= config.app %>/bower_components/lodash/dist',
-                        dest: '<%= config.dist %>/js',
-                        src: 'lodash.min.js'
-                    },
-                    {
-                        expand: true,
-                        cwd: 'worker',
-                        dest: '<%= config.dist %>/worker',
-                        src: '*'
-                    }
-                ]
+                files: [{
+                    expand: true,
+                    cwd: '<%= config.app %>',
+                    dest: '<%= config.dist %>',
+                    src: ['js/worker/*', 'img/**/*', 'templates/**/*', 'fonts/**/*', '*.html']
+                }, {
+                    expand: true,
+                    cwd: '<%= config.app %>/bower_components/bootstrap/dist/fonts',
+                    dest: '<%= config.dist %>/fonts',
+                    src: '*'
+                }, {
+                    expand: true,
+                    cwd: '<%= config.app %>/bower_components/requirejs',
+                    dest: '<%= config.dist %>/js',
+                    src: 'require.js'
+                }, {
+                    expand: true,
+                    cwd: '<%= config.app %>/bower_components/lodash/dist',
+                    dest: '<%= config.dist %>/js',
+                    src: 'lodash.min.js'
+                }, {
+                    expand: true,
+                    cwd: 'worker',
+                    dest: '<%= config.dist %>/worker',
+                    src: '*'
+                }]
             }
         },
         requirejs: {
@@ -225,16 +209,73 @@ module.exports = function (grunt) {
         grunt.file.write('app/js/options.js', processedTmpl);
     });
 
-    grunt.registerTask('postProcess', function(){
+    grunt.registerTask('checkoutWebsite', function () {
+        var done = this.async();
+        var childProcess = require('child_process');
+        var exec = childProcess.exec;
+        exec('rm -fr /tmp/gh-review.pages/ 2> /dev/null && git clone --reference ./ -b gh-pages git@github.com:Dica-Developer/gh-review.git /tmp/gh-review.pages', function (error, stdout, stderr) {
+            var result = true;
+            if (stdout) {
+                grunt.log.write(stdout);
+            }
+            if (stderr) {
+                grunt.log.write(stderr);
+            }
+            if (error !== null) {
+                grunt.log.error(error);
+                result = false;
+            }
+            done(result);
+        });
+    });
+
+    grunt.registerTask('cleanDeploy', function () {
+        var done = this.async();
+        var childProcess = require('child_process');
+        var exec = childProcess.exec;
+        exec('rm -r /tmp/gh-review.pages/*', function () {
+            done(true);
+        });
+    });
+
+    grunt.registerTask('commitAndPush', function () {
+        var done = this.async();
+        var childProcess = require('child_process');
+        var exec = childProcess.exec;
+        exec('cd /tmp/gh-review.pages && git add --all . && git commit -m "* deploy release" && git push', function (error, stdout, stderr) {
+            var result = true;
+            if (stdout) {
+                grunt.log.write(stdout);
+            }
+            if (stderr) {
+                grunt.log.write(stderr);
+            }
+            if (error !== null) {
+                grunt.log.error(error);
+                result = false;
+            }
+            done(result);
+        });
+    });
+
+    grunt.registerTask('postProcess', function () {
         var done = this.async();
         var fs = require('fs');
-        var indexHtml = fs.readFileSync('dist/index.html', {encoding: 'UTF8'});
+        var indexHtml = fs.readFileSync('dist/index.html', {
+            encoding: 'UTF8'
+        });
         indexHtml = indexHtml.replace('bower_components/requirejs/require.js', 'js/require.js');
-        fs.writeFileSync('dist/index.html', indexHtml, {encoding: 'UTF8'});
+        fs.writeFileSync('dist/index.html', indexHtml, {
+            encoding: 'UTF8'
+        });
 
-        var collectorJs = fs.readFileSync('dist/js/worker/collector.js', {encoding: 'UTF8'});
+        var collectorJs = fs.readFileSync('dist/js/worker/collector.js', {
+            encoding: 'UTF8'
+        });
         collectorJs = collectorJs.replace('../../bower_components/lodash/dist/lodash.min.js', '../lodash.min.js');
-        fs.writeFileSync('dist/js/worker/collector.js', collectorJs, {encoding: 'UTF8'});
+        fs.writeFileSync('dist/js/worker/collector.js', collectorJs, {
+            encoding: 'UTF8'
+        });
         done();
     });
 
@@ -249,7 +290,7 @@ module.exports = function (grunt) {
 
     grunt.registerTask('devWatch', [
         'jshint',
-//        'ngdocs',
+        //        'ngdocs',
         'less:dev',
         'copy:dev'
     ]);
@@ -257,7 +298,7 @@ module.exports = function (grunt) {
     grunt.registerTask('dev', [
         'clean:dev',
         'jshint',
-//        'ngdocs',
+        //        'ngdocs',
         'processTmpl:dev',
         'copy:dev',
         'less:dev',
@@ -275,4 +316,12 @@ module.exports = function (grunt) {
         'coveralls'
     ]);
 
+    grunt.registerTask('deploy', [
+        'karma:travis',
+        'dist',
+        'checkoutWebsite',
+        'cleanDeploy',
+        'copy:deploy',
+        'commitAndPush'
+    ]);
 };
