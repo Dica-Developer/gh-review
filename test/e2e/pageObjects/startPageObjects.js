952d0f6972a2df84f17909b2276d7e3e1471399c
Jörg Weber <weber.joerg.h@gmail.com>
2014-09-05 08:23:12 +0200

    * add user menu as page object

--- test/e2e/pageObjects/startPageObjects.js
+++ test/e2e/pageObjects/startPageObjects.js
@@ -1,4 +1,6 @@
 exports.HeaderMenu = function () {
+    'use strict';
+
     this.menuElements = element.all(by.name('ghr-top-menu-links'));
     this.reviewLink = this.menuElements.get(0);
     this.modulesLink = this.menuElements.get(1);
@@ -25,6 +27,48 @@ exports.HeaderMenu = function () {
             .then(function (visible) {
                 if(visible){
                     _this.reviewLink.all(by.tagName('a')).first().click();
+                }
+            });
+    };
+};
+
+exports.UserMenu = function () {
+    'use strict';
+
+    this.menuElements = element(by.id('userMenu')).all(by.tagName('a'));
+    this.menuEntries = element(by.id('userMenuDropdown')).all(by.tagName('a'));
+    this.menuDropdown = this.menuElements.get(0);
+    this.whoAmILink = this.menuElements.get(1);
+    this.bugLink = this.menuElements.get(2);
+    this.moreLink = this.menuElements.get(3);
+    this.howtoLink = this.menuElements.get(4);
+    this.aboutLink = this.menuElements.get(5);
+    this.logoutLink = this.menuElements.get(6);
+
+    this.getLinkText = function (link) {
+        return link.getText();
+    };
+
+    this.getLinkUrl = function (link) {
+        return link.getAttribute('href');
+    };
+
+    this.show = function () {
+        var _this = this;
+        this.whoAmILink.isDisplayed()
+            .then(function (visible) {
+                if(!visible){
+                    _this.menuDropdown.click();
+                }
+            });
+    };
+
+    this.hide = function () {
+        var _this = this;
+        this.whoAmILink.isDisplayed()
+            .then(function (visible) {
+                if(visible){
+                    _this.menuDropdown.click();
                 }
             });
     };
