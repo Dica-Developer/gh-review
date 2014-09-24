3f7ee33a84af5d8732bb0f1cfc9fe33fd8954303
Jörg Weber <weber.joerg.h@gmail.com>
2014-09-24 19:42:52 +0200

    #35
    * add up, down, left, right and enter key handler into commit list view
    * set location hash to current selected commit.sha to scroll into view

--- app/js/controller/CommitListController.js
+++ app/js/controller/CommitListController.js
@@ -3,10 +3,77 @@ define(['controllers', 'lodash', 'moment'], function (controllers, _, moment) {
   controllers
     .controller('CommitListController', [
       '$scope',
+      '$state',
       '$stateParams',
+      '$location',
       'commitsApproved',
       'filter',
-      function ($scope, $stateParams, commitsApproved, filter) {
+      'hotkeys',
+      function ($scope, $state, $stateParams, $location, commitsApproved, filter, hotkeys) {
+        var flattenedCommitList = [], currentCommitPointer = 0;
+
+        hotkeys.bindTo($scope)
+          .add({
+            combo: 'down',
+            description: 'Navigate through commits',
+            callback: function(event) {
+              event.preventDefault();
+              if(currentCommitPointer < flattenedCommitList.length -1){
+                currentCommitPointer++;
+                setSelectedCommit();
+              }
+            }
+          })
+          .add({
+            combo: 'up',
+            description: 'Navigate through commits',
+            callback: function(event) {
+              event.preventDefault();
+              if(currentCommitPointer > 0){
+                currentCommitPointer--;
+                setSelectedCommit();
+              }
+            }
+          })
+          .add({
+            combo: 'enter',
+            description: 'Navigate through commits',
+            callback: function(event) {
+              event.preventDefault();
+              $state.go('commitBySha', {
+                sha: flattenedCommitList[currentCommitPointer].sha,
+                user: filterById.getOwner(),
+                repo: filterById.getRepo()
+              });
+            }
+          })
+          .add({
+            combo: 'right',
+            description: 'Next page',
+            callback: function(event) {
+              event.preventDefault();
+              if(filterById.hasNextPage){
+                $scope.getNextPage();
+              }
+            }
+          })
+          .add({
+            combo: 'left',
+            description: 'Previous page',
+            callback: function(event) {
+              event.preventDefault();
+              if(filterById.hasPreviousPage){
+                $scope.getPreviousPage();
+              }
+            }
+          });
+
+        function setSelectedCommit () {
+          var sha = flattenedCommitList[currentCommitPointer].sha;
+          $scope.selectedCommit = sha;
+          $location.hash(sha);
+        }
+
         $scope.loader = '';
         $scope.hasNext = false;
         $scope.hasPrevious = false;
@@ -31,14 +98,19 @@ define(['controllers', 'lodash', 'moment'], function (controllers, _, moment) {
             return moment(commit.commit.committer.date).format('YYYY-MM-DD');
           });
 
-          return _.sortBy(groupedCommits, function(a, b){
+          var groupedSortedCommits = _.sortBy(groupedCommits, function(a, b){
             return moment(a).isBefore(b);
           });
+
+          flattenedCommitList = _.flatten(groupedSortedCommits);
+
+          return groupedSortedCommits;
         }
 
         filterById.getCommits(0, 20).then(function (commits) {
           $scope.sortedGroupedCommits = getSortedCommits(commits);
           setButtonStates();
+          setSelectedCommit();
         });
 
         $scope.getNextPage = function () {
