d3e58a7ba7c7072583556accdcdb439eb7be28ba
Jörg Weber <weber.joerg.h@gmail.com>
2014-10-03 18:41:45 +0200

    * add own controller to commit list directive * pass filter and commit list to commit list directive scope

--- app/js/directives/CommitList.js
+++ app/js/directives/CommitList.js
@@ -1,119 +1,122 @@
 (function (angular) {
   'use strict';
   angular.module('GHReview')
-    .directive('ghreviewCommitList', ['$location', '$state', 'moment', '_', 'hotkeys',
-      function ($location, $state, moment, _, hotkeys) {
+    .directive('ghreviewCommitList', [
+      function () {
         return {
           restrict: 'E',
           templateUrl: 'templates/commitList.html',
-          transclude: true,
-          link: function ($scope, element, attr) {
-            var flattenedCommitList = [], currentCommitPointer = -1, filter;
+          scope: {
+            'commits': '=commitList',
+            'filter': '='
+          },
+          controller: [
+            '$scope',
+            '$location',
+            '$state',
+            'moment',
+            '_',
+            'hotkeys',
+            function ($scope, $location, $state, moment, _, hotkeys) {
+              var flattenedCommitList = [], currentCommitPointer = -1;
 
-            hotkeys.bindTo($scope)
-              .add({
-                combo: 'down',
-                description: 'Navigate through commits',
-                callback: function (event) {
-                  event.preventDefault();
-                  if (currentCommitPointer < flattenedCommitList.length - 1) {
-                    currentCommitPointer++;
-                    setSelectedCommit();
+              hotkeys.bindTo($scope)
+                .add({
+                  combo: 'down',
+                  description: 'Navigate through commits',
+                  callback: function (event) {
+                    event.preventDefault();
+                    if (currentCommitPointer < flattenedCommitList.length - 1) {
+                      currentCommitPointer++;
+                      setSelectedCommit();
+                    }
                   }
-                }
-              })
-              .add({
-                combo: 'up',
-                description: 'Navigate through commits',
-                callback: function (event) {
-                  event.preventDefault();
-                  if (currentCommitPointer > 0) {
-                    currentCommitPointer--;
-                    setSelectedCommit();
+                })
+                .add({
+                  combo: 'up',
+                  description: 'Navigate through commits',
+                  callback: function (event) {
+                    event.preventDefault();
+                    if (currentCommitPointer > 0) {
+                      currentCommitPointer--;
+                      setSelectedCommit();
+                    }
                   }
-                }
-              })
-              .add({
-                combo: 'enter',
-                description: 'Navigate through commits',
-                callback: function (event) {
-                  event.preventDefault();
-                  if (currentCommitPointer > -1) {
-                    var sha = flattenedCommitList[currentCommitPointer].sha;
-                    $scope.loader = sha;
-                    $state.go('commitBySha', {
-                      sha: sha,
-                      user: filter.getOwner(),
-                      repo: filter.getRepo()
-                    });
+                })
+                .add({
+                  combo: 'enter',
+                  description: 'Navigate through commits',
+                  callback: function (event) {
+                    event.preventDefault();
+                    if (currentCommitPointer > -1) {
+                      var sha = flattenedCommitList[currentCommitPointer].sha;
+                      $scope.loader = sha;
+                      $state.go('commitBySha', {
+                        sha: sha,
+                        user: $scope.filter.getOwner(),
+                        repo: $scope.filter.getRepo()
+                      });
+                    }
                   }
-                }
-              })
-              .add({
-                combo: 'right',
-                description: 'Next page',
-                callback: function (event) {
-                  event.preventDefault();
-                  if (filter.hasNextPage) {
-                    $scope.getNextPage();
+                })
+                .add({
+                  combo: 'right',
+                  description: 'Next page',
+                  callback: function (event) {
+                    event.preventDefault();
+                    if ($scope.filter.hasNextPage) {
+                      $scope.getNextPage();
+                    }
                   }
-                }
-              })
-              .add({
-                combo: 'left',
-                description: 'Previous page',
-                callback: function (event) {
-                  event.preventDefault();
-                  if (filter.hasPreviousPage) {
-                    $scope.getPreviousPage();
+                })
+                .add({
+                  combo: 'left',
+                  description: 'Previous page',
+                  callback: function (event) {
+                    event.preventDefault();
+                    if ($scope.filter.hasPreviousPage) {
+                      $scope.getPreviousPage();
+                    }
                   }
+                });
+
+              function setSelectedCommit() {
+                if (flattenedCommitList.length > 0) {
+                  var sha = flattenedCommitList[currentCommitPointer].sha;
+                  $scope.selectedCommit = sha;
+                  $location.hash(sha);
                 }
-              });
-
-            function setSelectedCommit() {
-              if (flattenedCommitList.length > 0) {
-                var sha = flattenedCommitList[currentCommitPointer].sha;
-                $scope.selectedCommit = sha;
-                $location.hash(sha);
-              }
-            }
-
-            function getSortedCommits(commits) {
-              var groupedCommits = _.groupBy(commits, function (commit) {
-                return moment(commit.commit.committer.date).format('YYYY-MM-DD');
-              });
-
-              var groupedSortedCommits = _.sortBy(groupedCommits, function (a, b) {
-                return moment(a).isBefore(b);
-              });
-
-              flattenedCommitList = _.flatten(groupedSortedCommits);
-
-              var existingShaHash = $location.hash();
-              if (existingShaHash !== '') {
-                currentCommitPointer = _.findIndex(flattenedCommitList, {sha: existingShaHash});
-                $scope.selectedCommit = existingShaHash;
-                $location.hash(existingShaHash);
               }
 
-              return groupedSortedCommits;
-            }
+              this.setSortedCommits = function () {
+                var groupedCommits = _.groupBy($scope.commits, function (commit) {
+                  return moment(commit.commit.committer.date).format('YYYY-MM-DD');
+                });
 
-            var setButtonStates = function () {
-              $scope.hasNext = filter.hasNextPage;
-              $scope.hasPrevious = filter.hasPreviousPage;
-              $scope.hasFirst = filter.hasFirstPage;
-            };
+                var groupedSortedCommits = _.sortBy(groupedCommits, function (a, b) {
+                  return moment(a).isBefore(b);
+                });
 
-            $scope.$watch(attr.commitList, function (commitList) {
-              $scope.sortedCommits = getSortedCommits(commitList);
+                flattenedCommitList = _.flatten(groupedSortedCommits);
+
+                var existingShaHash = $location.hash();
+                if (existingShaHash !== '') {
+                  currentCommitPointer = _.findIndex(flattenedCommitList, {sha: existingShaHash});
+                  $scope.selectedCommit = existingShaHash;
+                  $location.hash(existingShaHash);
+                }
+
+                $scope.sortedCommits = groupedSortedCommits;
+              };
+
+              $scope.hasNext = $scope.filter.hasNextPage;
+              $scope.hasPrevious = $scope.filter.hasPreviousPage;
+              $scope.hasFirst = $scope.filter.hasFirstPage;
+            }],
+          link: function ($scope, element, attr, controller) {
+            $scope.$watch(attr.commits, function () {
+              controller.setSortedCommits();
             });
-
-            $scope.$watch(attr.filter, function (newFilter) {
-              filter = newFilter;
-              setButtonStates();
-            });
-
           }
         };
       }
