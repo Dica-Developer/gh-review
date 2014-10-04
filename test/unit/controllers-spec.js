ff74ed62c677d1a9ae684b4619cd21ef03c72e4f
Jörg Weber <weber.joerg.h@gmail.com>
2014-10-04 20:43:46 +0200

    Add tests for:
    * RootController
    * LoginController

--- test/unit/controllers-spec.js
+++ test/unit/controllers-spec.js
@@ -270,4 +270,55 @@ describe('#Controller', function () {
     });
 
   });
+
+  describe('RootController', function () {
+
+    it('Should go to state "listFilter" if at least one filter is defined', inject(function($injector){
+      var state = $injector.get('$state');
+      var stateSpy = spyOn(state, 'go');
+      var filterSpy = {getAll: function(){
+        return [1,2];
+      }};
+      var $rootScope = $injector.get('$rootScope');
+      var $scope = $rootScope.$new();
+      var $controller = $injector.get('$controller');
+      $controller('RootController', {
+        '$scope': $scope,
+        'filter': filterSpy
+      });
+      expect(stateSpy).toHaveBeenCalledWith('listFilter');
+    }));
+
+    it('Should go to state "addFilter" if no filter is defined', inject(function($injector){
+      var state = $injector.get('$state');
+      var stateSpy = spyOn(state, 'go');
+      var filterSpy = {getAll: function(){
+        return [];
+      }};
+      var $rootScope = $injector.get('$rootScope');
+      var $scope = $rootScope.$new();
+      var $controller = $injector.get('$controller');
+      $controller('RootController', {
+        '$scope': $scope,
+        'filter': filterSpy
+      });
+      expect(stateSpy).toHaveBeenCalledWith('addFilter');
+    }));
+
+  });
+
+  describe('LoginController', function () {
+
+    it('Should set window.location.href to correct value', inject(function ($injector) {
+      var $windowSpy = {location: {href: null}};
+      var $rootScope = $injector.get('$rootScope');
+      var $scope = $rootScope.$new();
+      var $controller = $injector.get('$controller');
+      $controller('LoginController', {
+        '$scope': $scope,
+        '$window': $windowSpy
+      });
+      expect($windowSpy.location.href).toBe('https://github.com/login/oauth/authorize?client_id=5082108e53d762d90c00&redirect_uri=http://localhost:9000/oauth&scope=user, repo');
+    }));
+  });
 });
