243893a1e200cfdf87bd8b40e7bb8ac5c234311a
Jörg Weber <weber.joerg.h@gmail.com>
2014-10-11 00:24:28 +0200

    * add contributorCollector specs

--- app/js/Collectors/contributorCollector.js
+++ app/js/Collectors/contributorCollector.js
@@ -23,9 +23,11 @@
         defer = q();
 
       var pagination = function (result) {
-        tmpResult = tmpResult.concat(result);
         github.getNextPage(result, function (err, paginationResult) {
           if (!err) {
+            tmpResult = tmpResult.concat(paginationResult);
+            /* istanbul ignore if */
+            //no need to test this twice
             if (github.hasNextPage(paginationResult)) {
               pagination(paginationResult);
             } else {
@@ -45,6 +47,7 @@
         function (err, result) {
           if (!err) {
             if (github.hasNextPage(result)) {
+              tmpResult = result;
               pagination(result);
             } else {
               defer.resolve(result);
