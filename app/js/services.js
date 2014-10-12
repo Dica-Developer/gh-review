e176fb77064543cdecba378b8d09fd82c037aadf
Jörg Weber <weber.joerg.h@gmail.com>
2014-10-12 12:17:10 +0200

    * remove authenticated check, this is handled by router

--- app/js/services.js
+++ app/js/services.js
@@ -259,38 +259,34 @@
       return function (sha, user, repo) {
         var defer = $q.defer();
 
-        if (authenticated.get()) {
-          githubUserData.get()
-            .then(function (userData) {
-              var commitState = {
-                version: options.ghReview.version,
-                approved: true,
-                approver: userData.login,
-                approvalDate: Date.now()
-              };
-              var comment = '```json\n' + JSON.stringify(commitState, null, 2) + '\n```\napproved with [gh-review](http://gh-review.herokuapp.com/)';
-              github.repos.createCommitComment({
-                user: user,
-                repo: repo,
-                // TODO sha and commit id are the same. Why do we need both?
-                sha: sha,
-                /*jshint camelcase:false*/
-                commit_id: sha,
-                body: comment
-              }, function (error, comment) {
-                if (!error) {
-                  commentCollector.addApprovalComment(sha, comment.id);
-                  defer.resolve();
-                } else {
-                  defer.reject(error);
-                }
-              });
-            }).catch(function (error) {
-              defer.reject(error);
+        githubUserData.get()
+          .then(function (userData) {
+            var commitState = {
+              version: options.ghReview.version,
+              approved: true,
+              approver: userData.login,
+              approvalDate: Date.now()
+            };
+            var comment = '```json\n' + JSON.stringify(commitState, null, 2) + '\n```\napproved with [gh-review](http://gh-review.herokuapp.com/)';
+            github.repos.createCommitComment({
+              user: user,
+              repo: repo,
+              // TODO sha and commit id are the same. Why do we need both?
+              sha: sha,
+              /*jshint camelcase:false*/
+              commit_id: sha,
+              body: comment
+            }, function (error, comment) {
+              if (!error) {
+                commentCollector.addApprovalComment(sha, comment.id);
+                defer.resolve();
+              } else {
+                defer.reject(error);
+              }
             });
-        } else {
-          defer.reject(new Error('Not authenticated'));
-        }
+          }).catch(function (error) {
+            defer.reject(error);
+          });
         return defer.promise;
       };
     }
