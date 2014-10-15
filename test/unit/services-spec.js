9d5eb1547808b2486dc19e198623315596a4bb72
Jörg Weber <weber.joerg.h@gmail.com>
2014-10-15 15:55:27 +0200

    * move service tests into test suite

--- test/unit/services-spec.js
+++ test/unit/services-spec.js
@@ -1,4 +1,4 @@
-/*global inject*/
+/*global inject, Github*/
 
 describe('#Services', function () {
   'use strict';
@@ -46,7 +46,7 @@ describe('#Services', function () {
       localStorage.clear();
     });
 
-    it('Should store access token to local storage', function () {
+    it('Should be defined', function () {
       expect(github instanceof Github).toBeTruthy();
     });
 
@@ -607,103 +607,101 @@ describe('#Services', function () {
       expect(commitProviderService).toEqual(commitProvider);
     });
   });
-});
 
-describe('.isCommentNotApprovalComment', function () {
-  'use strict';
+  describe('.isCommentNotApprovalComment', function () {
 
-  var commentCollector, isCommentNotApprovalComment;
+    var commentCollector, isCommentNotApprovalComment;
 
-  beforeEach(inject(function ($injector) {
-    commentCollector = $injector.get('commentCollector');
-    isCommentNotApprovalComment = $injector.get('isCommentNotApprovalComment');
-  }));
+    beforeEach(inject(function ($injector) {
+      commentCollector = $injector.get('commentCollector');
+      isCommentNotApprovalComment = $injector.get('isCommentNotApprovalComment');
+    }));
 
-  it('Should return false', function () {
-    spyOn(commentCollector, 'getApproveComments').and.returnValue({
-      commentIsApprovalComment: true
+    it('Should return false', function () {
+      spyOn(commentCollector, 'getApproveComments').and.returnValue({
+        commentIsApprovalComment: true
+      });
+      expect(isCommentNotApprovalComment('commentIsApprovalComment')).toBe(false);
     });
-    expect(isCommentNotApprovalComment('commentIsApprovalComment')).toBe(false);
+
+    it('Should return true', function () {
+      spyOn(commentCollector, 'getApproveComments').and.returnValue({
+        commentIsNotApprovalComment: false
+      });
+      expect(isCommentNotApprovalComment('commentIsNotApprovalComment')).toBe(true);
+    });
+
+    it('Should return true', function () {
+      spyOn(commentCollector, 'getApproveComments').and.returnValue({
+        commentIsApprovalComment: true
+      });
+      expect(isCommentNotApprovalComment('commentIsNotApprovalComment')).toBe(true);
+    });
   });
 
-  it('Should return true', function () {
-    spyOn(commentCollector, 'getApproveComments').and.returnValue({
-      commentIsNotApprovalComment: false
+  describe('.isCommentApprovalCommentFromUser', function () {
+
+    var commentCollector, isCommentApprovalCommentFromUser;
+
+    beforeEach(inject(function ($injector) {
+      commentCollector = $injector.get('commentCollector');
+      isCommentApprovalCommentFromUser = $injector.get('isCommentApprovalCommentFromUser');
+    }));
+
+    it('Should return false comment is approval comment but user is not approver', function () {
+      spyOn(commentCollector, 'getApproveComments').and.returnValue({
+        commentIsApprovalComment: true
+      });
+      expect(isCommentApprovalCommentFromUser({
+        id: 'isCommentApprovalCommentFromUser',
+        user: {
+          login: 'ms'
+        }
+      }, {
+        login: 'karl'
+      })).toBe(false);
     });
-    expect(isCommentNotApprovalComment('commentIsNotApprovalComment')).toBe(true);
-  });
 
-  it('Should return true', function () {
-    spyOn(commentCollector, 'getApproveComments').and.returnValue({
-      commentIsApprovalComment: true
+    it('Should return false comment is not approval comment and user is not approver', function () {
+      spyOn(commentCollector, 'getApproveComments').and.returnValue({
+        commentIsNotApprovalComment: false
+      });
+      expect(isCommentApprovalCommentFromUser({
+        id: 'notApprovalComment',
+        user: {
+          login: 'ms'
+        }
+      }, {
+        login: 'karl'
+      })).toBe(false);
     });
-    expect(isCommentNotApprovalComment('commentIsNotApprovalComment')).toBe(true);
-  });
-});
 
-describe('.isCommentApprovalCommentFromUser', function () {
-  'use strict';
-
-  var commentCollector, isCommentApprovalCommentFromUser;
-
-  beforeEach(inject(function ($injector) {
-    commentCollector = $injector.get('commentCollector');
-    isCommentApprovalCommentFromUser = $injector.get('isCommentApprovalCommentFromUser');
-  }));
-
-  it('Should return false comment is approval comment but user is not approver', function () {
-    spyOn(commentCollector, 'getApproveComments').and.returnValue({
-      commentIsApprovalComment: true
-    });
-    expect(isCommentApprovalCommentFromUser({
-      id: 'isCommentApprovalCommentFromUser',
-      user: {
+    it('Should return false comment is not approval comment but user is approver', function () {
+      spyOn(commentCollector, 'getApproveComments').and.returnValue({
+        commentIsApprovalComment: true
+      });
+      expect(isCommentApprovalCommentFromUser({
+        id: 'commentIsNotApprovalComment',
+        user: {
+          login: 'ms'
+        }
+      }, {
         login: 'ms'
-      }
-    }, {
-      login: 'karl'
-    })).toBe(false);
-  });
-
-  it('Should return false comment is not approval comment and user is not approver', function () {
-    spyOn(commentCollector, 'getApproveComments').and.returnValue({
-      commentIsNotApprovalComment: false
+      })).toBe(false);
     });
-    expect(isCommentApprovalCommentFromUser({
-      id: 'notApprovalComment',
-      user: {
-        login: 'ms'
-      }
-    }, {
-      login: 'karl'
-    })).toBe(false);
-  });
 
-  it('Should return false comment is not approval comment but user is approver', function () {
-    spyOn(commentCollector, 'getApproveComments').and.returnValue({
-      commentIsApprovalComment: true
-    });
-    expect(isCommentApprovalCommentFromUser({
-      id: 'commentIsNotApprovalComment',
-      user: {
+    it('Should return true comment is approval comment and user is approver', function () {
+      spyOn(commentCollector, 'getApproveComments').and.returnValue({
+        commentIsApprovalComment: true
+      });
+      expect(isCommentApprovalCommentFromUser({
+        id: 'commentIsApprovalComment',
+        user: {
+          login: 'ms'
+        }
+      }, {
         login: 'ms'
-      }
-    }, {
-      login: 'ms'
-    })).toBe(false);
-  });
-
-  it('Should return true comment is approval comment and user is approver', function () {
-    spyOn(commentCollector, 'getApproveComments').and.returnValue({
-      commentIsApprovalComment: true
+      })).toBe(true);
     });
-    expect(isCommentApprovalCommentFromUser({
-      id: 'commentIsApprovalComment',
-      user: {
-        login: 'ms'
-      }
-    }, {
-      login: 'ms'
-    })).toBe(true);
   });
 });
