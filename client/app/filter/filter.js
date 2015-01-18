f146f0a0ac5d88d6a4f893ac0242a3e48c5d6268
Jörg Weber <weber.joerg.h@gmail.com>
2015-01-18 22:38:32 +0100

    Add filter as module to app folder
    * add new / remove old controller file
    * add main js file with routes
    * add style file
    * add new / remove old template file
    * add controller spec file

---
+++ client/app/filter/filter.js
@@ -0,0 +1,27 @@
+(function(angular){
+  'use strict';
+
+  angular.module('GHReview')
+    .config(function ($stateProvider) {
+      $stateProvider
+        .state('addFilter', {
+          url: '/filter/add',
+          templateUrl: 'app/filter/filter.html',
+          controller: 'FilterController'
+        })
+        .state('editFilter', {
+          url: '/filter/edit/{filterId}',
+          templateUrl: 'app/filter/filter.html',
+          controller: 'FilterController'
+        })
+        .state('commitsByFilter', {
+          url: '/filter/{filterId}/commits',
+          templateUrl: 'app/filter/filter.html',
+          controller: 'FilterController',
+          resolve: {
+            commitsApproved: 'getCommitApproved'
+          }
+        });
+    });
+}(angular));
+
