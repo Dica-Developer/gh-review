7937dd692f141cc7bdac3f736c676d6b32193797
Jörg Weber <weber.joerg.h@gmail.com>
2015-01-18 23:42:21 +0100

    * remove duplicate spec

--- test/unit/controllers-spec.js
+++ test/unit/controllers-spec.js
@@ -102,21 +102,6 @@ describe('#Controller', function () {
 
   });
 
-  describe('LoginController', function () {
-
-    it('Should set window.location.href to correct value', inject(function ($injector) {
-      var $windowSpy = {location: {href: null}};
-      var $rootScope = $injector.get('$rootScope');
-      var $scope = $rootScope.$new();
-      var $controller = $injector.get('$controller');
-      $controller('LoginController', {
-        '$scope': $scope,
-        '$window': $windowSpy
-      });
-      expect($windowSpy.location.href).toBe('https://github.com/login/oauth/authorize?client_id=5082108e53d762d90c00&redirect_uri=http://localhost:9000/oauth/&scope=user, repo');
-    }));
-  });
-
   describe('MenuDirectiveController', function () {
 
     describe('$scope.name', function(){
