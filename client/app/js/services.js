1c958599a6fe872d2848df5a572688668f6c29cb
Jörg Weber <weber.joerg.h@gmail.com>
2015-01-19 23:16:06 +0100

    * move filter service to components

--- client/app/js/services.js
+++ client/app/js/services.js
@@ -5,42 +5,6 @@
 
   var services = angular.module('GHReview');
 
-  services.factory('filter', ['_', 'localStorageService', 'filterProvider',
-    function (_, localStorageService, filterProvider) {
-
-      var getAll = function () {
-        var filter = [];
-        var filterIds = localStorageService.get('filter');
-        if (filterIds !== null) {
-          filterIds.split(',').forEach(function (id) {
-            filter.push(filterProvider.get(id));
-          });
-        }
-        return filter;
-      };
-
-      var getById = function (filterId) {
-        return filterProvider.get(filterId);
-      };
-
-      var remove = function (filterId) {
-        localStorageService.remove('filter-' + filterId);
-        var filterList = localStorageService.get('filter').split(',');
-        _.remove(filterList, function (value) {
-          return value === filterId;
-        });
-        localStorageService.set('filter', filterList.join(','));
-      };
-
-      return {
-        getAll: getAll,
-        getById: getById,
-        remove: remove
-      };
-
-    }
-  ]);
-
   services.factory('getCommitApproved', ['commentCollector',
     function (commentCollector) {
       return commentCollector.getCommitApproved();
