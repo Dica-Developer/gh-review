1c6fc7ef022cef0c20ede204789505ed3e78f7a0
Jörg Weber <weber.joerg.h@gmail.com>
2015-01-20 23:17:44 +0100

    * add build task

--- Gruntfile.js
+++ Gruntfile.js
@@ -171,4 +171,23 @@ module.exports = function (grunt) {
     'copy:deploy',
     'commitAndPush'
   ]);
+
+  grunt.registerTask('build', [
+    'clean:dist',
+    'injector:less',
+    'concurrent:dist',
+    'injector',
+    'wiredep',
+    'useminPrepare',
+    'autoprefixer',
+    'ngtemplates',
+    'concat',
+    'ngAnnotate',
+    'copy:dist',
+    'cdnify',
+    'cssmin',
+    'uglify',
+    'rev',
+    'usemin'
+  ]);
 };
