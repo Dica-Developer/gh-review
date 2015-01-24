117062dc58eb67b154b493d19fd87bc6a772af2e
Jörg Weber <weber.joerg.h@gmail.com>
2015-01-24 10:28:34 +0100

    * add custom nightwatch commands for login and submenu handling

---
+++ test/helper/nightwatch-custom-commands/subMenu.js
@@ -0,0 +1,16 @@
+(function () {
+  'use strict';
+
+  exports.command = function (menuEntry) {
+    var self = this;
+
+    self
+      .click('.dropdown-toggle.ng-binding')
+      .click('.dropdown-toggle.ng-binding')
+      .click('#submenu-' + menuEntry);
+
+    return this; // allows the command to be chained.
+  };
+
+
+}());
