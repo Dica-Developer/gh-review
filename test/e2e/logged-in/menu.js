b850773f93034caeb71f0f8f50e327c85960ed09
Jörg Weber <weber.joerg.h@gmail.com>
2015-01-24 20:33:56 +0100

    * optimize test to use one open browser instance to perform all tests

--- test/e2e/logged-in/menu.js
+++ test/e2e/logged-in/menu.js
@@ -7,6 +7,9 @@
         .url('http://localhost:9000')
         .login();
     },
+    'after': function(browser){
+      browser.end();
+    },
     'Menu entries': function (browser) {
       browser
         .assert.elementPresent('#menu-commits')
@@ -27,46 +30,39 @@
         .assert.elementPresent('#submenu-about')
         .assert.containsText('#submenu-about', 'About')
         .assert.elementPresent('#submenu-logout')
-        .assert.containsText('#submenu-logout', 'Logout')
-        .end();
+        .assert.containsText('#submenu-logout', 'Logout');
     },
     'Menu entry "Modules"': function (browser) {
       browser
         .click('#menu-modules')
         .waitForElementVisible('#searchValue', 5000)
-        .assert.urlContains('#/filter/modules')
-        .end();
+        .assert.urlContains('#/filter/modules');
     },
     'Menu entry "Who Am I"': function (browser) {
       browser
         .subMenu('whoami')
-        .assert.urlContains('#/whoami')
-        .end();
+        .assert.urlContains('#/whoami');
     },
     'Menu entry "I found a bug"': function (browser) {
       browser
         .subMenu('bug')
-        .assert.urlContains('https://github.com/Dica-Developer/gh-review/issues')
-        .end();
+        .assert.urlContains('https://github.com/Dica-Developer/gh-review/issues');
     },
     'Menu entry "I need more"': function (browser) {
       browser
         .subMenu('more')
-        .assert.urlContains('https://github.com/Dica-Developer/gh-review')
-        .end();
+        .assert.urlContains('https://github.com/Dica-Developer/gh-review');
     },
     'Menu entry "How to use"': function (browser) {
       browser
         .subMenu('howto')
-        .assert.urlContains('https://github.com/Dica-Developer/gh-review/wiki')
-        .end();
+        .assert.urlContains('https://github.com/Dica-Developer/gh-review/wiki');
     },
     'Menu entry "Logout"': function (browser) {
       browser
         .subMenu('logout')
         .assert.urlContains('#/welcome')
-        .assert.elementPresent('#loginLogoutContainer')
-        .end();
+        .assert.elementPresent('#loginLogoutContainer');
     }
   };
 
