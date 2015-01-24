bc3b585340ae80999427fb4ace671524c0893739
Jörg Weber <weber.joerg.h@gmail.com>
2015-01-24 11:14:10 +0100

    * add selenium jar manually downloading it seems to encounter problems on travis

--- grunt/nightwatch.js
+++ grunt/nightwatch.js
@@ -4,7 +4,8 @@
   module.exports = {
     options: {
       standalone: true,
-      jar_url: 'http://selenium-release.storage.googleapis.com/2.44/selenium-server-standalone-2.44.0.jar',
+      //jar_url: 'http://selenium-release.storage.googleapis.com/2.44/selenium-server-standalone-2.44.0.jar',
+      jar_path: '<%= config.test %>/helper/selenium-server-standalone-2.44.0.jar',
       src_folders: ['<%= config.test %>/e2e'],
       output_folder: '<%= config.test %>/report',
       custom_commands_path: '<%= config.test %>/helper/nightwatch-custom-commands',
