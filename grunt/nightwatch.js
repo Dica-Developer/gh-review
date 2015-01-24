aa150d69221520febb3dcdf59ca4d5bba43b6c35
Jörg Weber <weber.joerg.h@gmail.com>
2015-01-24 10:26:42 +0100

    * add a nightwatch task

---
+++ grunt/nightwatch.js
@@ -0,0 +1,15 @@
+/*jshint camelcase:false*/
+(function () {
+  'use strict';
+  module.exports = {
+    options: {
+      standalone: true,
+      jar_url: 'http://selenium-release.storage.googleapis.com/2.44/selenium-server-standalone-2.44.0.jar',
+      src_folders: ['<%= config.test %>/e2e'],
+      output_folder: '<%= config.test %>/report',
+      custom_commands_path: '<%= config.test %>/helper/nightwatch-custom-commands',
+      test_settings: {},
+      selenium: {}
+    }
+  };
+}());
