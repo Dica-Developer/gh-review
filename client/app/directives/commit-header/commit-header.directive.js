1b5d47d0a38cc7d79100927c813738c7ae7a840c
Jörg Weber <weber.joerg.h@gmail.com>
2015-01-25 09:16:38 +0100

    * combine commit header directive templates to one file

--- client/app/directives/commit-header/commit-header.directive.js
+++ client/app/directives/commit-header/commit-header.directive.js
@@ -5,19 +5,15 @@
   angular.module('GHReview')
     .directive('commitHeader', [
       function () {
-        var shouldBeCollabsible = false;
         return {
           restrict: 'E',
-          templateUrl: function () {
-            var tmpBasePath = 'app/directives/commit-header/';
-            return shouldBeCollabsible ? tmpBasePath + 'commit-header-collabsible.html' : tmpBasePath + 'commit-header.html';
-          },
+          templateUrl: 'app/directives/commit-header/commit-header-collabsible.html',
           link: function ($scope, element, attr) {
             $scope.$watch(attr.commit, function (value) {
               var message = value.message;
               var splittedMessage = message.split('\n');
               var firstLine = splittedMessage[0];
-              shouldBeCollabsible = splittedMessage.length > 1 || firstLine.length >= maxLengthForFirstLine;
+              $scope.commitHeaderStatus.shouldCollapse = splittedMessage.length > 1 || firstLine.length >= maxLengthForFirstLine;
             });
           }
         };
