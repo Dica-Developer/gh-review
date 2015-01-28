f118bb8a7b5b9812fbdbd18bec5f5ef7143d5604
Jörg Weber <weber.joerg.h@gmail.com>
2015-01-28 23:12:06 +0100

    * add Commit.factory spec

---
+++ client/components/commit/Commit.factory.spec.js
@@ -0,0 +1,237 @@
+describe('Factory: Commit', function () {
+  'use strict';
+
+  beforeEach(module('GHReview'));
+  beforeEach(module('commitMockModule'));
+  beforeEach(module('commentMockModule'));
+
+  var Commit, commitsMock, commentsMock, commits, comments, $q, $rootScope, $interval, github;
+
+  beforeEach(inject(function ($injector) {
+    Commit = $injector.get('Commit');
+    commitsMock = $injector.get('commitsMock');
+    commentsMock = $injector.get('commentsMock');
+    commits = $injector.get('commits');
+    comments = $injector.get('comments');
+    $q = $injector.get('$q');
+    $rootScope = $injector.get('$rootScope');
+    $interval = $injector.get('$interval');
+    github = $injector.get('github');
+  }));
+
+  it('should be defined', function () {
+    expect(Commit).toBeDefined();
+  });
+
+
+  describe('.getCommit', function () {
+    var commit;
+
+    beforeEach(function () {
+      commit = new Commit();
+    });
+
+    it('Should add a well named cache object', function () {
+      spyOn(github.repos, 'getCommit');
+      commit.getCommit({user: 'TestUser', repo: 'testRepo', sha: 'testSha'});
+      expect(commit.getCommit.cache).toBeDefined();
+      expect(commit.getCommit.cache['TestUser-testRepo-testSha']).toBeDefined();
+    });
+
+    it('Should invalidate cache after a given time', function () {
+      var cacheExpireTime = 10 * 60 * 1000; //10min
+
+      spyOn(github.repos, 'getCommit');
+      commit.getCommit({user: 'TestUser', repo: 'testRepo', sha: 'testSha'});
+      expect(commit.getCommit.cache).toBeDefined();
+      expect(commit.getCommit.cache['TestUser-testRepo-testSha']).toBeDefined();
+      $interval.flush(cacheExpireTime);
+      expect(commit.getCommit.cache['TestUser-testRepo-testSha']).not.toBeDefined();
+    });
+  });
+
+  describe('.getFiles', function () {
+
+    var commit;
+
+    beforeEach(function () {
+      commit = new Commit({user: 'TestUser', repo: 'testRepo', sha: 'testSha'});
+    });
+
+    it('should call .getCommit', function () {
+      spyOn(commit, 'getCommit').and.returnValue($q.when());
+
+      commit.getFiles();
+
+      expect(commit.getCommit).toHaveBeenCalledWith({user: 'TestUser', repo: 'testRepo', sha: 'testSha'});
+    });
+
+    it('should return a promise', function () {
+      spyOn(commit, 'getCommit').and.returnValue($q.when());
+
+      var expectedPromise = commit.getFiles();
+
+      expect(expectedPromise.then).toBeDefined();
+    });
+
+    it('should return resolve with files object', function (done) {
+      spyOn(commit, 'getCommit').and.returnValue($q.when(commitsMock[3]));
+
+      commit.getFiles()
+        .then(function (files) {
+          expect(files).toEqual(jasmine.any(Array));
+          expect(files[0].lines).toBeDefined();
+          expect(files[0].name).toBeDefined();
+          done();
+        });
+
+      $rootScope.$apply();
+    });
+
+
+  });
+
+  describe('.approve', function () {
+    var commit;
+    beforeEach(function () {
+      commit = new Commit({user: 'TestUser', repo: 'testRepo', sha: 'testSha'});
+    });
+
+    it('should call comments.addCommitComment', function () {
+      spyOn(comments, 'addCommitComment').and.returnValue($q.when());
+      commit.approve({login: 'me'});
+      expect(comments.addCommitComment).toHaveBeenCalled();
+    });
+
+    it('should call comments.addCommitComment with correct arguments', function () {
+      spyOn(comments, 'addCommitComment').and.returnValue($q.when());
+      commit.approve({login: 'me'});
+      var callArgs = comments.addCommitComment.calls.argsFor(0);
+      expect(callArgs[0]).toBe('testSha');
+      expect(callArgs[1]).toBe('TestUser');
+      expect(callArgs[2]).toBe('testRepo');
+      expect(callArgs[3]).toMatch(new RegExp('approved with \\[gh-review\\]\\(http://gh-review.herokuapp.com/\\)', 'gm'));
+      expect(callArgs[3]).toMatch(new RegExp('"approver": "me"', 'gm'));
+    });
+
+    it('should return promise', function () {
+      spyOn(comments, 'addCommitComment').and.returnValue($q.when());
+      var expectedPromise = commit.approve({login: 'me'});
+      expect(expectedPromise.then).toBeDefined();
+    });
+
+  });
+
+  describe('.unapprove', function () {
+    var commit;
+    beforeEach(function () {
+      spyOn(github.repos, 'deleteCommitComment');
+      spyOn(comments, 'getForCommit').and.returnValue($q.when(commentsMock[0]));
+      commit = new Commit({user: 'TestUser', repo: 'testRepo', sha: 'testSha'});
+      commit.getComments();
+      $rootScope.$apply();
+    });
+
+    it('should return a promise', function () {
+      var expectedPromise = commit.unapprove({login: 'JayGray'});
+      expect(expectedPromise.then).toBeDefined();
+    });
+
+    it('should call Comment#remove', function () {
+      var comment = commit.comments.commitComments[0];
+      spyOn(comment, 'remove');
+      commit.unapprove({login: 'JayGray'});
+      expect(comment.remove).toHaveBeenCalled();
+    });
+  });
+
+  describe('.getComments', function () {
+    var commit;
+    beforeEach(function () {
+      commit = new Commit({user: 'TestUser', repo: 'testRepo', sha: 'testSha'});
+    });
+
+    it('should return promise', function () {
+      spyOn(comments, 'getForCommit').and.returnValue($q.when(commentsMock[0]));
+      var expectedPromise = commit.getComments();
+      expect(expectedPromise.then).toBeDefined();
+    });
+
+    it('should resolve with splitted line and commit comments', function (done) {
+      spyOn(comments, 'getForCommit').and.returnValue($q.when(commentsMock[0]));
+      commit.getComments()
+        .then(function (comments) {
+          expect(comments.lineComments).toBeDefined();
+          expect(comments.commitComments).toBeDefined();
+
+          expect(comments.lineComments.length).toBe(2);
+          expect(comments.commitComments.length).toBe(1);
+          done();
+        });
+      $rootScope.$apply();
+    });
+
+    it('should add comments as property to Commit', function (done) {
+      spyOn(comments, 'getForCommit').and.returnValue($q.when(commentsMock[0]));
+      commit.getComments()
+        .then(function () {
+          expect(commit.comments.lineComments).toBeDefined();
+          expect(commit.comments.commitComments).toBeDefined();
+
+          expect(commit.comments.lineComments.length).toBe(2);
+          expect(commit.comments.commitComments.length).toBe(1);
+          done();
+        });
+      $rootScope.$apply();
+    });
+  });
+
+  describe('.getApprover', function(){
+    var commit;
+    beforeEach(function () {
+      spyOn(github.repos, 'deleteCommitComment');
+      spyOn(comments, 'getForCommit').and.returnValue($q.when(commentsMock[0]));
+      commit = new Commit({user: 'TestUser', repo: 'testRepo', sha: 'testSha'});
+      commit.getComments();
+      $rootScope.$apply();
+    });
+
+    it('should return a list of approver if Commit.comments not undefined', function(){
+      var approvers = commit.getApprover();
+      expect(approvers).toEqual(jasmine.any(Array));
+      expect(approvers.length).toBe(1);
+      expect(approvers[0]).toBe('JayGray');
+    });
+
+    it('should return empty list if Commit.comments undefined', function(){
+      delete commit.comments;
+      var approvers = commit.getApprover();
+      expect(approvers).toEqual(jasmine.any(Array));
+      expect(approvers.length).toBe(0);
+    });
+  });
+
+  describe('.isApprover', function(){
+    var commit;
+    beforeEach(function () {
+      spyOn(github.repos, 'deleteCommitComment');
+      spyOn(comments, 'getForCommit').and.returnValue($q.when(commentsMock[0]));
+      commit = new Commit({user: 'TestUser', repo: 'testRepo', sha: 'testSha'});
+      commit.getComments();
+      $rootScope.$apply();
+    });
+
+    it('should return true if given user.login match approve comment', function(){
+      expect(commit.isApprovedByUser({login: 'JayGray'})).toBe(true);
+    });
+
+    it('should return false if given user.login does not match approve comment', function(){
+      expect(commit.isApprovedByUser({login: 'NoMatch'})).toBe(false);
+    });
+
+    it('should return false no argument is given', function(){
+      expect(commit.isApprovedByUser()).toBe(false);
+    });
+  });
+
+});
