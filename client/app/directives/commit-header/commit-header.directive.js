d46560ec666eca98998f3f1d33f72f30966a852f
Jörg Weber <weber.joerg.h@gmail.com>
2015-01-28 13:56:43 +0100

    * add commit header specific scope vars to the directive instead of any controller

--- client/app/directives/commit-header/commit-header.directive.js
+++ client/app/directives/commit-header/commit-header.directive.js
@@ -9,11 +9,17 @@
           restrict: 'E',
           templateUrl: 'app/directives/commit-header/commit-header-collabsible.html',
           link: function ($scope, element, attr) {
+            $scope.commitHeaderStatus = {
+              open: false,
+              shouldCollapse: false
+            };
             $scope.$watch(attr.commit, function (value) {
-              var message = value.message;
-              var splittedMessage = message.split('\n');
-              var firstLine = splittedMessage[0];
-              $scope.commitHeaderStatus.shouldCollapse = splittedMessage.length > 1 || firstLine.length >= maxLengthForFirstLine;
+              if(value){
+                var message = value.message;
+                var splittedMessage = message.split('\n');
+                var firstLine = splittedMessage[0];
+                $scope.commitHeaderStatus.shouldCollapse = splittedMessage.length > 1 || firstLine.length >= maxLengthForFirstLine;
+              }
             });
           }
         };
