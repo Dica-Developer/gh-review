1e482777b927f7ec3f7948ce31385499ebede82c
Jörg Weber <weber.joerg.h@gmail.com>
2015-01-28 13:46:17 +0100

    * use original commit response from github instead of manipulated

--- client/app/directives/avatar/avatar.directive.js
+++ client/app/directives/avatar/avatar.directive.js
@@ -7,11 +7,14 @@
         restrict: 'E',
         templateUrl: 'app/directives/avatar/avatar.html',
         link: function ($scope, element, attr) {
-          $scope.$watch(attr.commit, function (value) {
-            $scope.name = value.name;
-            $scope.imgLink = value.avatar;
-            $scope.link = value.committerLink || '#';
           $scope.imgLink = 'assets/images/icon-social-github-128.png';
+          $scope.$watch(attr.commit, function (commitResponse) {
+            if(commitResponse){
+              $scope.name = commitResponse.commit.committer.name;
+              /*jshint camelcase:false*/
+              $scope.imgLink = commitResponse.committer.avatar_url;
+              $scope.link = commitResponse.committer.html_url || '#';
+            }
           });
         }
       };
