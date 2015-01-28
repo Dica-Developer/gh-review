0355d25403e15afe03bcac34aefd823d59cddcab
Jörg Weber <weber.joerg.h@gmail.com>
2015-01-28 13:53:26 +0100

    * change github commits factory to an actual service

--- client/components/github/github-commits.service.js
+++ client/components/github/github-commits.service.js
@@ -2,38 +2,44 @@
   'use strict';
 
   angular.module('GHReview')
-    .factory('commits', ['$q', 'github', 'localStorageService',
+    .service('commits', ['$q', 'github', 'localStorageService',
       function ($q, github, localStorageService) {
-        return {
-          bySha: function (params) {
-            var defer = $q.defer();
-            github.repos.getCommit({
-              user: params.user,
-              repo: params.repo,
-              sha: params.sha
-            }, function (error, res) {
-              if (error) {
-                defer.reject(error);
-              } else {
-                defer.resolve(res);
+        this.bySha = function (params) {
+          var defer = $q.defer();
+          github.repos.getCommit({
+            user: params.user,
+            repo: params.repo,
+            sha: params.sha,
+            headers: {
+              'Accept': 'application/vnd.github-commitcomment.html+json'
+            }
+          }, function (error, res) {
+            if (error) {
+              defer.reject(error);
+            } else {
+              if (res.meta) {
+                delete res.meta;
               }
-            });
-            return defer.promise;
-          },
-          byPath: /*istanbul ignore next*/ function (options) {
-            var defer = $q.defer();
-            var commitsPerFileWorker = new Worker('worker/commitsOfFile.js');
-            commitsPerFileWorker.onmessage = function (event) {
-              if ('commits' === event.data.type) {
-                commitsPerFileWorker.terminate();
-                defer.resolve(event.data.commits.concat([]));
-              }
-            };
-            options.type = 'getCommits';
-            options.token = localStorageService.get('accessToken');
-            commitsPerFileWorker.postMessage(options);
-            return defer.promise;
-          }
+              defer.resolve(res);
+            }
+          });
+          return defer.promise;
+        };
+
+        /*istanbul ignore next*/
+        this.byPath = function (options) {
+          var defer = $q.defer();
+          var commitsPerFileWorker = new Worker('worker/commitsOfFile.js');
+          commitsPerFileWorker.onmessage = function (event) {
+            if ('commits' === event.data.type) {
+              commitsPerFileWorker.terminate();
+              defer.resolve(event.data.commits.concat([]));
+            }
+          };
+          options.type = 'getCommits';
+          options.token = localStorageService.get('accessToken');
+          commitsPerFileWorker.postMessage(options);
+          return defer.promise;
         };
       }
     ]);
