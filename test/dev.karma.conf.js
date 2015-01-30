2b6c224666d4d5d6e6bff4aa50283788a6d224d4
Jörg Weber <weber.joerg.h@gmail.com>
2015-01-30 21:16:18 +0100

    * remove unnecessary extra karma conf file

--- test/dev.karma.conf.js
+++ test/dev.karma.conf.js
@@ -26,7 +26,8 @@ module.exports = function (config) {
       'client/components/**/*.html',
       '.tmp/options.js',
       'test/helper/commitMockModule.js',
-      'test/helper/commentMockModule.js'
+      'test/helper/commentMockModule.js',
+      'test/helper/bind-polyfill.js'
     ],
     exclude: [
       'client/worker/*.*'
