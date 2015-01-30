30271c44e2cedb58cfd42d978e12ced2401adf12
Jörg Weber <weber.joerg.h@gmail.com>
2015-01-30 22:22:49 +0100

    * add specs for github services (commits, comments)

---
+++ client/components/github/github-comments.service.spec.js
@@ -0,0 +1,104 @@
+describe('Service: github-comment', function () {
+  'use strict';
+
+  beforeEach(module('GHReview'));
+
+  var github, $rootScope, comments;
+
+  beforeEach(inject(function ($injector) {
+    github = $injector.get('github');
+    $rootScope = $injector.get('$rootScope');
+    comments = $injector.get('comments');
+  }));
+
+  describe('.getForCommit', function(){
+
+    it('should call github.repos.getCommitComments', function () {
+      spyOn(github.repos, 'getCommitComments');
+      comments.getForCommit({});
+      expect(github.repos.getCommitComments).toHaveBeenCalled();
+    });
+
+    it('should return promise and resolve if data exist', function (done) {
+      spyOn(github.repos, 'getCommitComments');
+      comments.getForCommit({})
+        .then(function (data) {
+          expect(data).toBeDefined();
+          expect(data.result).toBe('testResult');
+          done();
+        });
+      var callback = github.repos.getCommitComments.calls.argsFor(0)[1];
+      callback(null, { result: 'testResult' });
+      expect(github.repos.getCommitComments).toHaveBeenCalled();
+      $rootScope.$apply();
+    });
+
+    it('should remove meta property from response', function (done) {
+      spyOn(github.repos, 'getCommitComments');
+      comments.getForCommit({})
+        .then(function (data) {
+          expect(data).toBeDefined();
+          expect(data.meta).not.toBeDefined();
+          done();
+        });
+      var callback = github.repos.getCommitComments.calls.argsFor(0)[1];
+      callback(null, { result: 'testResult', meta: true });
+      expect(github.repos.getCommitComments).toHaveBeenCalled();
+      $rootScope.$apply();
+    });
+
+    it('should return promise and reject if error exist', function (done) {
+      spyOn(github.repos, 'getCommitComments');
+      comments.getForCommit({})
+        .then(null, function (error) {
+          expect(error).toBeDefined();
+          expect(error.name).toBe('Error');
+          done();
+        });
+      var callback = github.repos.getCommitComments.calls.argsFor(0)[1];
+      callback({ name: 'Error' }, null);
+      expect(github.repos.getCommitComments).toHaveBeenCalled();
+      $rootScope.$apply();
+    });
+
+  });
+
+  describe('.addCommitComment', function(){
+
+    it('should call github.repos.createCommitComment', function () {
+      spyOn(github.repos, 'createCommitComment');
+      comments.addCommitComment({});
+      expect(github.repos.createCommitComment).toHaveBeenCalled();
+    });
+
+    it('should return promise and resolve error is null', function (done) {
+      spyOn(github.repos, 'createCommitComment');
+      comments.addCommitComment({})
+        .then(function (data) {
+          expect(data).toBeDefined();
+          expect(data.result).toBe('testResult');
+          done();
+        });
+      var callback = github.repos.createCommitComment.calls.argsFor(0)[1];
+      callback(null, { result: 'testResult' });
+      expect(github.repos.createCommitComment).toHaveBeenCalled();
+      $rootScope.$apply();
+    });
+
+    it('should return promise and reject if error exist', function (done) {
+      spyOn(github.repos, 'createCommitComment');
+      comments.addCommitComment({})
+        .then(null, function (error) {
+          expect(error).toBeDefined();
+          expect(error.name).toBe('Error');
+          done();
+        });
+      var callback = github.repos.createCommitComment.calls.argsFor(0)[1];
+      callback({ name: 'Error' }, null);
+      expect(github.repos.createCommitComment).toHaveBeenCalled();
+      $rootScope.$apply();
+    });
+
+  });
+
+});
---
+++ client/components/github/github-commits.service.spec.js
@@ -0,0 +1,66 @@
+describe('Service: github-commit', function () {
+  'use strict';
+
+  beforeEach(module('GHReview'));
+
+  var github, $rootScope, commits;
+
+  beforeEach(inject(function ($injector) {
+    github = $injector.get('github');
+    $rootScope = $injector.get('$rootScope');
+    commits = $injector.get('commits');
+  }));
+
+  describe('.bySha', function(){
+
+    it('should call github.repos.getCommit', function () {
+      spyOn(github.repos, 'getCommit');
+      commits.bySha({});
+      expect(github.repos.getCommit).toHaveBeenCalled();
+    });
+
+    it('should return promise and resolve if data exist', function (done) {
+      spyOn(github.repos, 'getCommit');
+      commits.bySha({})
+        .then(function (data) {
+          expect(data).toBeDefined();
+          expect(data.result).toBe('testResult');
+          done();
+        });
+      var callback = github.repos.getCommit.calls.argsFor(0)[1];
+      callback(null, { result: 'testResult' });
+      expect(github.repos.getCommit).toHaveBeenCalled();
+      $rootScope.$apply();
+    });
+
+    it('should remove meta property from response', function (done) {
+      spyOn(github.repos, 'getCommit');
+      commits.bySha({})
+        .then(function (data) {
+          expect(data).toBeDefined();
+          expect(data.meta).not.toBeDefined();
+          done();
+        });
+      var callback = github.repos.getCommit.calls.argsFor(0)[1];
+      callback(null, { result: 'testResult', meta: true });
+      expect(github.repos.getCommit).toHaveBeenCalled();
+      $rootScope.$apply();
+    });
+
+    it('should return promise and reject if error exist', function (done) {
+      spyOn(github.repos, 'getCommit');
+      commits.bySha({})
+        .then(null, function (error) {
+          expect(error).toBeDefined();
+          expect(error.name).toBe('Error');
+          done();
+        });
+      var callback = github.repos.getCommit.calls.argsFor(0)[1];
+      callback({ name: 'Error' }, null);
+      expect(github.repos.getCommit).toHaveBeenCalled();
+      $rootScope.$apply();
+    });
+
+  });
+
+});
