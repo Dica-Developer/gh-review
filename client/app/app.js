51d3f53960667d822a2355aa22d5a74b4e60a25a
Jörg Weber <weber.joerg.h@gmail.com>
2015-02-01 21:24:42 +0100

    * fix test by using app.config instead of app.run   to start the commentCollector (cherry picked from commit f8e42259f59c4771a974451e5a319fd4f86ad165)

--- client/app/app.js
+++ client/app/app.js
@@ -10,22 +10,24 @@
     'ngSanitize',
     'ngAnimate',
     'hljs'
-  ]).run(['authenticated', 'collectComments', function (authenticated, collectComments) {
-    if(authenticated.get()){
-      collectComments();
-    }
-  }]).config(['localStorageServiceProvider', '$urlRouterProvider',
+  ]).config(['localStorageServiceProvider', '$urlRouterProvider',
     function (localStorageServiceProvider, $urlRouterProvider) {
       localStorageServiceProvider.setPrefix('ghreview');
+      var commentCollectorStarted = false;
 
       $urlRouterProvider
         .otherwise('/')
         .rule(function ($injector, $location) {
           var auth = $injector.get('authenticated'),
+            collectComments = $injector.get('collectComments'),
             authenticated = auth.get(),
             path = $location.path();
 
           if ((path === '/' || path === '/login' || path === '/welcome') || authenticated) {
+            if(!commentCollectorStarted){
+              commentCollectorStarted = true;
+              collectComments();
+            }
             return $location.abbsUrl;
           } else {
             return $location.replace().path('/welcome');
