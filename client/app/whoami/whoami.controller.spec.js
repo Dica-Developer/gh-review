97ae64696204aa138c1e412d81cb6f8137becb79
Jörg Weber <weber.joerg.h@gmail.com>
2015-02-19 14:34:30 +0100

    * refactor spec to avoid actual github calls

--- client/app/whoami/whoami.controller.spec.js
+++ client/app/whoami/whoami.controller.spec.js
@@ -5,56 +5,45 @@
 
     beforeEach(module('GHReview'));
 
-    var WhoAmIController, $scope, $controller, githubUserDataSpy, ghUser, github;
+    var whoAmIController, $scope, $controller, ghUser, github;
 
     beforeEach(inject(function ($injector) {
       localStorage.setItem('ghreview.accessToken', 'test-to-ken');
       ghUser = $injector.get('ghUser');
       $controller = $injector.get('$controller');
       github = $injector.get('github');
+      spyOn(github.user, 'get');
       var $rootScope = $injector.get('$rootScope');
       $scope = $rootScope.$new();
+      whoAmIController = $controller('WhoAmIController', {
+        $scope: $scope
+      });
     }));
 
     afterEach(function () {
       localStorage.clear();
+      github.user.get.calls.reset();
     });
 
     it('Should be defined', function () {
-      githubUserDataSpy = spyOn(ghUser, 'get').and.returnValue({
-        then: function () {
-        }
-      });
-      WhoAmIController = $controller('WhoAmIController', {
-        $scope: $scope
-      });
-      expect(WhoAmIController).toBeDefined();
+      expect(whoAmIController).toBeDefined();
     });
 
     it('Should call ghUser', function () {
-      githubUserDataSpy = spyOn(ghUser, 'get').and.returnValue({
-        then: function () {
-        }
+      var githubUserDataSpy = spyOn(ghUser, 'get').and.returnValue({
+        then: function () {}
       });
-      WhoAmIController = $controller('WhoAmIController', {
+      whoAmIController = $controller('WhoAmIController', {
         $scope: $scope
       });
       expect(githubUserDataSpy).toHaveBeenCalled();
     });
 
     it('Should call github.user.get', function () {
-      spyOn(github.user, 'get');
-      WhoAmIController = $controller('WhoAmIController', {
-        $scope: $scope
-      });
       expect(github.user.get).toHaveBeenCalled();
     });
 
     it('Should apply response of github.user.get to $scope', function () {
-      spyOn(github.user, 'get');
-      WhoAmIController = $controller('WhoAmIController', {
-        $scope: $scope
-      });
       var getCallback = github.user.get.calls.argsFor(0)[1];
       getCallback(null, {
         login: 'testUser',
