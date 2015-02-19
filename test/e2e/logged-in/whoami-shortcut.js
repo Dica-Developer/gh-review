4735b13b800344afd8f9d284819456832e92fc8e
Jörg Weber <weber.joerg.h@gmail.com>
2015-02-19 16:25:24 +0100

    * use login helper in whoami shortcut test

--- test/e2e/logged-in/whoami-shortcut.js
+++ test/e2e/logged-in/whoami-shortcut.js
@@ -5,11 +5,8 @@
     'WhoAmI per shortcut': function (browser) {
       browser
         .url('http://localhost:9000')
+        .login()
         .waitForElementVisible('body', 1000)
-        .execute(function(){
-          localStorage.setItem('ghreview.accessToken', '44046cd4b4b85afebfe3ccaec13fd8c08cc80aad');
-          return true;
-        })
         .click('.navbar-brand')
         .keys(['g', 'w'])
         .assert.urlContains('whoami')
