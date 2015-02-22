afe619e93a600ab29516f1c41057c5b584c15501
Jörg Weber <weber.joerg.h@gmail.com>
2015-02-22 23:28:46 +0100

    * give app a little more to time to respond in e2e tests

--- test/e2e/logged-in/whoami-shortcut.js
+++ test/e2e/logged-in/whoami-shortcut.js
@@ -6,9 +6,11 @@
       browser
         .url('http://localhost:9000')
         .login()
-        .waitForElementVisible('body', 1000)
         .click('.navbar-brand')
+        .waitForElementVisible('body', 1000)
+        .pause(2000)
         .keys(['g', 'w'])
+        .waitForElementVisible('ul.list-group', 5000)
         .assert.urlContains('whoami')
         .end();
     }
