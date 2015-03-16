d1fb67a7116f232edcea085dfd2fdc3f5b2e9cd8
Jörg Weber <weber.joerg.h@gmail.com>
2015-03-16 21:53:56 +0100

    * remove old files

--- client/components/events/events.service.mock.js
+++
@@ -1,9 +0,0 @@
-(function(angular){
-  'use strict';
-  angular.module('eventsMock', [])
-    .factory('events', function() {
-      return {
-          getAll: function() {}
-      };
-    });
-}(angular));
