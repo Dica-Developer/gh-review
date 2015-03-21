d2a276c52457b87a4019d093628a3de36cd95a19
Jörg Weber <weber.joerg.h@gmail.com>
2015-03-21 14:44:01 +0100

    * remove comments

--- client/components/file/File.factory.js
+++ client/components/file/File.factory.js
@@ -5,25 +5,7 @@
     .factory('File', ['$injector',
       function ($injector) {
 
-        //var lines = file.patch ? file.patch.split(/\r?\n/) : null,
-        ///*jshint camelcase: false*/
-        //  start = file.blob_url.indexOf('blob/') + 'blob/'.length,
-        //  shaAndPath = file.blob_url.substr(start),
-        //  end = shaAndPath.indexOf('/'),
-        //  blobSha = shaAndPath.substr(0, end);
-        //
-        //return {
-        //  lines: lines ? new Chunk(lines, file.filename) : null,
-        //  name: file.filename,
-        //  blobSha: blobSha,
-        //  additions: file.additions,
-        //  deletions: file.deletions,
-        //  changes: file.changes,
-        //  status: file.status
-        //};
-
-        var /*$q = $injector.get('$q'),*/
-          _ = $injector.get('_'),
+        var _ = $injector.get('_'),
           Chunk = $injector.get('Chunk');
 
 
