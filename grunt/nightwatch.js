c0081407ada5a412e92e31a4847ee984bf54a092
Jörg Weber <weber.joerg.h@gmail.com>
2015-12-24 06:46:58 +0100

    * update selenium standalone server

--- grunt/nightwatch.js
+++ grunt/nightwatch.js
@@ -4,7 +4,7 @@
   module.exports = {
     options: {
       standalone: true,
-      jar_path: '<%= config.test %>/helper/selenium-server-standalone-2.44.0.jar',
+      jar_path: '<%= config.test %>/helper/selenium-server-standalone-2.48.2.jar',
       src_folders: ['<%= config.test %>/e2e'],
       output_folder: '<%= config.test %>/report',
       custom_commands_path: '<%= config.test %>/helper/nightwatch-custom-commands',
